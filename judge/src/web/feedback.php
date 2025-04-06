effc30f608e13a3d51f5e3e2f11389618babead3
Son Seung Min <sonsmmm@naver.com>
2025-04-07 02:10:10 +0900

    feedback button update

--- judge/src/web/feedback.php
+++ judge/src/web/feedback.php
@@ -3,20 +3,20 @@
 include("template/$OJ_TEMPLATE/header.php");
 include("include/db_info.inc.php"); // 데이터베이스 연결을 위한 파일
 
-// submission_id 값 가져오기
-$submission_id = isset($_GET['submission_id']) ? intval($_GET['submission_id']) : 0;
+// solution_id 값 가져오기
+$solution_id = isset($_GET['submission_id']) ? intval($_GET['submission_id']) : 0;
 
-if ($submission_id > 0) {
-    // submission_id에 해당하는 피드백 조회
-    $sql = "SELECT feedback FROM solution WHERE submission_id = ?";
+if ($solution_id > 0) {
+    // solution_id에 해당하는 피드백 조회
+    $sql = "SELECT feedback FROM solution WHERE solution_id = ?"; // submission_id -> solution_id로 변경
     $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param("i", $submission_id);
+    $stmt->bind_param("i", $solution_id); // solution_id 사용
     $stmt->execute();
     $stmt->bind_result($feedback);
     $stmt->fetch();
     $stmt->close();
 } else {
-    // submission_id가 없을 경우 오류 메시지
+    // solution_id가 없을 경우 오류 메시지
     $feedback = "피드백을 찾을 수 없습니다.";
 }
 
