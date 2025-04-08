c4d3bb5d7b02457ad2b7f0ecbfa6053ed24271c4
Son Seung Min <sonsmmm@naver.com>
2025-04-09 01:01:14 +0900

    paragrpahfeedback complete fix(2)

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -6,31 +6,33 @@ $sid = isset($_GET['solution_id']) ? $_GET['solution_id'] : null;
 include("template/$OJ_TEMPLATE/header.php");
 
 if (!$sid) {
-    echo "<div class='ui warning message'><div class='header'>solution_id가 없습니다.</div></div>";
-    include("template/$OJ_TEMPLATE/footer.php");
-    exit;
+  echo "<div class='ui warning message'><div class='header'>solution_id가 없습니다.</div></div>";
+  include("template/$OJ_TEMPLATE/footer.php");
+  exit;
 }
 
-// 예시 문단
-$paragraphs = [
-    [
-        'description' => 'printf를 이용하여 Hello World 출력하기',
-        'answer' => 'printf("Hello World");',
-    ]
+// ✅ 문단 정답 정의
+$correct_paragraphs = [
+    'printf("Hello World");'
+];
+
+$descriptions = [
+    'printf를 이용하여 Hello World 출력하기'
 ];
 
 $user_inputs = [];
 $results = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach ($paragraphs as $index => $block) {
-        $input = $_POST["para_$index"] ?? '';
-        $user_inputs[$index] = $input;
+  foreach ($correct_paragraphs as $i => $answer) {
+    $input = $_POST["para_$i"] ?? '';
+    $user_inputs[$i] = $input;
 
-        $normalized_input = preg_replace('/\s+/', '', $input);
-        $normalized_answer = preg_replace('/\s+/', '', $block['answer']);
-        $results[$index] = ($normalized_input === $normalized_answer);
-    }
+    $normalized_input = preg_replace('/\s+/', '', $input);
+    $normalized_answer = preg_replace('/\s+/', '', $answer);
+
+    $results[$i] = ($normalized_input === $normalized_answer);
+  }
 }
 
 include("template/$OJ_TEMPLATE/paragraphfeedback.php");
