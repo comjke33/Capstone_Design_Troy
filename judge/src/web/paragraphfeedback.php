aab81329687d02e2099e7b587107414361c41267
Son Seung Min <sonsmmm@naver.com>
2025-04-09 00:05:15 +0900

    paraph Q fix(4)

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -1,6 +1,6 @@
 <?php
 $OJ_TEMPLATE = "syzoj";
-$show_title = "한 문단 피드백";
+$show_title = "문단 피드백";
 $sid = isset($_GET['solution_id']) ? $_GET['solution_id'] : null;
 
 include("template/$OJ_TEMPLATE/header.php");
@@ -11,26 +11,31 @@ if (!$sid) {
     exit;
 }
 
-// 예시 정답 줄
-$correct_lines = [
+// 예시 문단
+$paragraphs = [
     [
-    'printf("Hello World");'
+        'description' => '1. 사용자로부터 양의 정수 n 입력 받기',
+        'answer' => "int n;\nscanf(\"%d\", &n);",
+        'hint' => "scanf 사용 시 주소 연산자 & 가 필요합니다. 변수 선언은 소문자 int 입니다."
+    ],
+    [
+        'description' => '2. 팩토리얼 값(fact) 계산하기',
+        'answer' => "int fact = 1;\nfor(int i = 1; i <= n; i++) {\n    fact *= i;\n}",
+        'hint' => "for 문은 1부터 n까지 반복해야 하고, fact는 누적 곱으로 계산됩니다."
     ]
 ];
 
-$descriptions = [
-    'printf를 이용하여 Hello World 출력하기'
-];
-
-// 사용자 입력 처리
 $user_inputs = [];
 $results = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach ($correct_lines as $index => $answer) {
-        $input = $_POST["line_$index"] ?? '';
+    foreach ($paragraphs as $index => $block) {
+        $input = $_POST["para_$index"] ?? '';
         $user_inputs[$index] = $input;
-        $results[$index] = trim($input) === $answer;
+
+        $normalized_input = preg_replace('/\s+/', '', $input);
+        $normalized_answer = preg_replace('/\s+/', '', $block['answer']);
+        $results[$index] = ($normalized_input === $normalized_answer);
     }
 }
 
