db399e8302109f3b45cd99b500db6bacb7a3f37b
Son Seung Min <sonsmmm@naver.com>
2025-04-09 00:04:38 +0900

    paraph Q fix(3)

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -1,6 +1,6 @@
 <?php
 $OJ_TEMPLATE = "syzoj";
-$show_title = "문단 피드백";
+$show_title = "한 문단 피드백";
 $sid = isset($_GET['solution_id']) ? $_GET['solution_id'] : null;
 
 include("template/$OJ_TEMPLATE/header.php");
@@ -11,25 +11,26 @@ if (!$sid) {
     exit;
 }
 
-// 예시 문단
-$paragraphs = [
+// 예시 정답 줄
+$correct_lines = [
     [
-        'description' => 'printf를 이용하여 Hello World 출력하기',
-        'answer' => "printf("Hello World");",
+    'printf("Hello World");'
     ]
 ];
 
+$descriptions = [
+    'printf를 이용하여 Hello World 출력하기'
+];
+
+// 사용자 입력 처리
 $user_inputs = [];
 $results = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach ($paragraphs as $index => $block) {
-        $input = $_POST["para_$index"] ?? '';
+    foreach ($correct_lines as $index => $answer) {
+        $input = $_POST["line_$index"] ?? '';
         $user_inputs[$index] = $input;
-
-        $normalized_input = preg_replace('/\s+/', '', $input);
-        $normalized_answer = preg_replace('/\s+/', '', $block['answer']);
-        $results[$index] = ($normalized_input === $normalized_answer);
+        $results[$index] = trim($input) === $answer;
     }
 }
 
