19cfeac4eca97d1dc540ef59a3f8963f98a12f4e
Son Seung Min <sonsmmm@naver.com>
2025-04-09 00:00:00 +0900

    paraph Q fix(2)

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -12,24 +12,26 @@ if (!$sid) {
 }
 
 // 예시 문단
-$correct_paragraphs = [
-    'printf("Hello World");'
-];
-
-$descriptions = [
-    'printf를 이용하여 Hello World 출력하기'
+$paragraphs = [
+    [
+        'description' => 'printf를 이용하여 Hello World 출력하기',
+        'answer' => "printf("Hello World");",
+    ]
 ];
 
 $user_inputs = [];
 $results = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach ($correct_paragraphs as $index => $answer) {
+    foreach ($paragraphs as $index => $block) {
         $input = $_POST["para_$index"] ?? '';
         $user_inputs[$index] = $input;
 
         $normalized_input = preg_replace('/\s+/', '', $input);
-        $normalized_answer = preg_replace('/\s+/', '', $answer);
+        $normalized_answer = preg_replace('/\s+/', '', $block['answer']);
         $results[$index] = ($normalized_input === $normalized_answer);
     }
 }
+
+include("template/$OJ_TEMPLATE/paragraphfeedback.php");
+include("template/$OJ_TEMPLATE/footer.php");
