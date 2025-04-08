873ca0eb90bbeda5842b759cbe823e82db5ec287
Son Seung Min <sonsmmm@naver.com>
2025-04-08 23:38:19 +0900

    linefeedback.php submit fix

--- judge/src/web/template/syzoj/linefeedback.php
+++ judge/src/web/template/syzoj/linefeedback.php
@@ -8,17 +8,17 @@
             <form method="post" class="ui form">
                 <?php foreach ($correct_lines as $i => $line): ?>
                     <div class="field step">
-                        <label><?= ($i+1) . '. ' . $descriptions[$i] ?>
+                        <label>
+                            <?= ($i+1) . '. ' . $descriptions[$i] ?>
                             <?php if (isset($results[$i]) && $results[$i]): ?> ✅<?php endif; ?>
                         </label>
-                        <textarea name="line_<?= $i ?>" rows="2"
-                                  class="ui textarea <?= isset($results[$i]) && $results[$i] ? 'correct' : '' ?>"
-                                  <?= isset($results[$i]) && $results[$i] ? 'readonly' : '' ?>><?= htmlspecialchars($user_inputs[$i] ?? '') ?></textarea>
-                        <button class="ui blue button" type="submit" name="submit" value="<?= $i ?>">제출</button>
 
-                        <?php if (isset($results[$i]) && !$results[$i] && !empty($user_inputs[$i])): ?>
-                            <div class="ui hint message">힌트: Scanf를 사용할 때는 변수 앞에 ~~</div>
-                        <?php endif; ?>
+                        <textarea name="line_<?= $i ?>" rows="2"
+                            class="ui textarea"
+                            style="<?= isset($results[$i]) && $results[$i] ? 'background-color: #f0f0f0;' : '' ?>"
+                            <?= isset($results[$i]) && $results[$i] ? 'readonly' : '' ?>><?= htmlspecialchars($user_inputs[$i] ?? '') ?></textarea>
+
+                        <button class="ui blue button" type="submit" name="submit" value="<?= $i ?>">제출</button>
                     </div>
                 <?php endforeach; ?>
             </form>
