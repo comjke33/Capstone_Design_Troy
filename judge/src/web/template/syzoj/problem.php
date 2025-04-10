a959b6aade2fde36b44cba9813517541cd83089d
Son Seung Min <sonsmmm@naver.com>
2025-04-11 01:03:21 +0900

    select level fix

--- judge/src/web/template/syzoj/problem.php
+++ judge/src/web/template/syzoj/problem.php
@@ -97,7 +97,7 @@ if(file_exists($solution_file)){
             if(!file_exists($OJ_DATA."/".$id."/solution.name")) echo "<a class='small ui primary button' href='#' onclick='transform()' role='button'>$MSG_SHOW_OFF</a>";
 
             // 단계별 풀기 버튼 추가
-            echo "<a class=\"small ui red button\" href=\"stepbystep.php?problem_id=$id\">단계별 풀기</a>";
+            echo "<a class=\"small ui red button\" href=\"selectlevel.php?problem_id=$id\">단계별 풀기</a>";
       ?>
 
 
