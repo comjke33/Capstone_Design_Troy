72a1163492308d5453595e5df858ed6cfacd2aec
Son Seung Min <sonsmmm@naver.com>
2025-04-12 02:49:31 +0900

    guideline2 UI

--- judge/src/web/guideline2.php
+++ judge/src/web/guideline2.php
@@ -8,7 +8,11 @@ preg_match_all("/rep_start\((.*?)\)(.*?)rep_end\((.*?)\)/s", $file_contents, $lo
 preg_match_all("/cond_start\((.*?)\)(.*?)cond_end\((.*?)\)/s", $file_contents, $conditionals);
 preg_match_all("/self_start\((.*?)\)(.*?)self_end\((.*?)\)/s", $file_contents, $self_blocks);
 
-// Render functions
+// Main container for all code blocks
+echo "<div class='main-container'>";
+
+// Render functions within a sub-container
+echo "<div class='functions-container'>";
 foreach ($functions[0] as $index => $function) {
     $func_name = extract_function_name($function);
     $function_content = $functions[2][$index];
@@ -17,8 +21,10 @@ foreach ($functions[0] as $index => $function) {
     echo "<p>" . nl2br($function_content) . "</p>";
     echo "</div>";
 }
+echo "</div>"; // End of functions-container
 
-// Render loops
+// Render loops within a sub-container
+echo "<div class='loops-container'>";
 foreach ($loops[0] as $index => $loop) {
     $loop_info = extract_loop_info($loop);
     $loop_content = $loops[2][$index];
@@ -27,8 +33,10 @@ foreach ($loops[0] as $index => $loop) {
     echo "<p>" . nl2br($loop_content) . "</p>";
     echo "</div>";
 }
+echo "</div>"; // End of loops-container
 
-// Render conditionals
+// Render conditionals within a sub-container
+echo "<div class='conditionals-container'>";
 foreach ($conditionals[0] as $index => $conditional) {
     $conditional_info = extract_conditional_info($conditional);
     $conditional_content = $conditionals[2][$index];
@@ -37,8 +45,10 @@ foreach ($conditionals[0] as $index => $conditional) {
     echo "<p>" . nl2br($conditional_content) . "</p>";
     echo "</div>";
 }
+echo "</div>"; // End of conditionals-container
 
-// Render self-contained blocks
+// Render self-contained blocks within a sub-container
+echo "<div class='self-blocks-container'>";
 foreach ($self_blocks[0] as $index => $self_block) {
     $self_block_info = extract_self_block_info($self_block);
     $self_block_content = $self_blocks[2][$index];
@@ -47,6 +57,10 @@ foreach ($self_blocks[0] as $index => $self_block) {
     echo "<p>" . nl2br($self_block_content) . "</p>";
     echo "</div>";
 }
+echo "</div>"; // End of self-blocks-container
+
+// Close main container
+echo "</div>"; // End of main-container
 
 // Helper functions for extracting details
 function extract_function_name($function) {
