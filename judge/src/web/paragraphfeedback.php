ae75aeb990581db9ae6964f179d0d5d85d5ba5da
Son Seung Min <sonsmmm@naver.com>
2025-04-11 23:24:25 +0900

    file upload error fix

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -11,15 +11,7 @@ if ($problem_id <= 0) {
 }
 
 $file_path = "/home/troy0012/aaa.txt";
-$file_handle = fopen($file_path, "r");
-if ($file_handle) {
-    while (($line = fgets($file_handle)) !== false) {
-        echo nl2br($line); // 줄바꿈을 HTML에서 보이도록 변환
-    }
-    fclose($file_handle);
-} else {
-    echo "파일을 열 수 없습니다.";
-}
 
+include $file_path;
 
 ?>
