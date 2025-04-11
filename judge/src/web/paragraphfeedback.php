2428c06d7561d61bc76270f0f5a56322fbb670f2
Son Seung Min <sonsmmm@naver.com>
2025-04-11 23:28:14 +0900

    file upload error fix

--- judge/src/web/paragraphfeedback.php
+++ judge/src/web/paragraphfeedback.php
@@ -10,14 +10,11 @@ if ($problem_id <= 0) {
     exit;
 }
 
-$file_path = "/home/troy0012/aaa.txt";
-$file_contents = file_get_contents($file_path);
-
 if ($file_contents === false) {
-    echo "파일을 읽을 수 없습니다.";
+  echo "파일을 읽을 수 없습니다.";
 } else {
-    var_dump($file_contents); // 파일 내용을 출력
-    echo nl2br($file_contents); // 줄바꿈을 HTML에서 보이도록 변환
+  var_dump($file_contents); // 파일 내용을 출력
+  echo nl2br($file_contents); // 줄바꿈을 HTML에서 보이도록 변환
 }
 
 ?>
