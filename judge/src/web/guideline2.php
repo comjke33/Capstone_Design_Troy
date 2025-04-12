f453d4baa32307561b086d4bea363ae3deb4e3ac
Son Seung Min <sonsmmm@naver.com>
2025-04-12 14:45:30 +0900

    guideline2 fix

--- judge/src/web/guideline2.php
+++ judge/src/web/guideline2.php
@@ -8,77 +8,48 @@ $file_path = "/home/Capstone_Design_Troy/test/test.txt";
 // 1. 파일 내용 읽기
 $file_contents = file_get_contents($file_path);
 
-// 2. 정규 표현식을 사용하여 각 블록을 구분합니다.
-// 'func_def_start'로 시작하고 'func_def_end'로 끝나는 함수 블록
-preg_match_all("/\[func_def_start\((.*?)\)\](.*?)\[func_def_end\((.*?)\)\]/s", $file_contents, $functions);
+// 2. 정규 표현식을 사용하여 각 블록을 색상별로 구분합니다.
+$patterns = [
+    'func_def' => "/\[func_def_start\((.*?)\)\](.*?)\[func_def_end\((.*?)\)\]/s",
+    'rep' => "/\[rep_start\((.*?)\)\](.*?)\[rep_end\((.*?)\)\]/s",
+    'cond' => "/\[cond_start\((.*?)\)\](.*?)\[cond_end\((.*?)\)\]/s",
+    'self' => "/\[self_start\((.*?)\)\](.*?)\[self_end\((.*?)\)\]/s",
+    'struct' => "/\[struct_start\((.*?)\)\](.*?)\[struct_end\((.*?)\)\]/s"
+];
 
-// 'rep_start'로 시작하고 'rep_end'로 끝나는 반복문 블록
-preg_match_all("/\[rep_start\((.*?)\)\](.*?)\[rep_end\((.*?)\)\]/s", $file_contents, $loops);
+// 3. 코드 내에서 [ ]로 감싸진 부분 제거
+$file_contents = preg_replace("/\[[^\]]*\]/", "", $file_contents); // [ ] 안의 내용 삭제
 
-// 'cond_start'로 시작하고 'cond_end'로 끝나는 조건문 블록
-preg_match_all("/\[cond_start\((.*?)\)\](.*?)\[cond_end\((.*?)\)\]/s", $file_contents, $conditionals);
-
-// 'self_start'로 시작하고 'self_end'로 끝나는 self-contained 블록
-preg_match_all("/\[self_start\((.*?)\)\](.*?)\[self_end\((.*?)\)\]/s", $file_contents, $self_blocks);
-
-// 'struct_start'로 시작하고 'struct_end'로 끝나는 구조체 블록
-preg_match_all("/\[struct_start\((.*?)\)\](.*?)\[struct_end\((.*?)\)\]/s", $file_contents, $structs);
-
-// 3. [ ] 안에 특정 태그가 포함된 부분을 제거
-$file_contents = preg_replace("/\[(rep_start|rep_end|self_start|self_end|func_def_start|func_def_end|cond_start|cond_end|struct_start|struct_end)\([^\)]*\)\]/", "", $file_contents);
-
-// 4. 각 블록을 색상별로 출력하기 전에 불필요한 부분을 제거한 후 출력합니다.
+// 4. 각 블록에 색상별로 출력하기
 echo "<div class='code-container'>";
 
-// 4.1 함수 블록 출력
-foreach ($functions[0] as $index => $function) {
-    $func_name = htmlspecialchars($functions[1][$index]);
-    $function_content = nl2br(htmlspecialchars($functions[2][$index]));
-    echo "<div class='code-block function'>";
-    echo "<h3>Function: $func_name</h3>";
-    echo "<p>$function_content</p>";
-    echo "</div>";
-}
+// 함수 블록을 찾아서 출력
+$file_contents = preg_replace_callback($patterns['func_def'], function($matches) {
+    return "<div class='code-block function' style='background-color: lightblue;'><h3>Function: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+}, $file_contents);
 
-// 4.2 반복문 블록 출력
-foreach ($loops[0] as $index => $loop) {
-    $loop_info = htmlspecialchars($loops[1][$index]);
-    $loop_content = nl2br(htmlspecialchars($loops[2][$index]));
-    echo "<div class='code-block loop'>";
-    echo "<h3>Loop: $loop_info</h3>";
-    echo "<p>$loop_content</p>";
-    echo "</div>";
-}
+// 반복문 블록을 찾아서 출력
+$file_contents = preg_replace_callback($patterns['rep'], function($matches) {
+    return "<div class='code-block loop' style='background-color: lightpink;'><h3>Loop: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+}, $file_contents);
 
-// 4.3 조건문 블록 출력
-foreach ($conditionals[0] as $index => $conditional) {
-    $conditional_info = htmlspecialchars($conditionals[1][$index]);
-    $conditional_content = nl2br(htmlspecialchars($conditionals[2][$index]));
-    echo "<div class='code-block conditional'>";
-    echo "<h3>Conditional: $conditional_info</h3>";
-    echo "<p>$conditional_content</p>";
-    echo "</div>";
-}
+// 조건문 블록을 찾아서 출력
+$file_contents = preg_replace_callback($patterns['cond'], function($matches) {
+    return "<div class='code-block conditional' style='background-color: lightgreen;'><h3>Conditional: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+}, $file_contents);
 
-// 4.4 self-contained 블록 출력
-foreach ($self_blocks[0] as $index => $self_block) {
-    $self_block_info = htmlspecialchars($self_blocks[1][$index]);
-    $self_block_content = nl2br(htmlspecialchars($self_blocks[2][$index]));
-    echo "<div class='code-block self-block'>";
-    echo "<h3>Self Block: $self_block_info</h3>";
-    echo "<p>$self_block_content</p>";
-    echo "</div>";
-}
+// self-contained 블록을 찾아서 출력
+$file_contents = preg_replace_callback($patterns['self'], function($matches) {
+    return "<div class='code-block self-block' style='background-color: lightyellow;'><h3>Self Block: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+}, $file_contents);
 
-// 4.5 구조체 블록 출력
-foreach ($structs[0] as $index => $struct) {
-    $struct_name = htmlspecialchars($struct[1]);
-    $struct_content = nl2br(htmlspecialchars($struct[2]));
-    echo "<div class='code-block struct'>";
-    echo "<h3>Struct: $struct_name</h3>";
-    echo "<p>$struct_content</p>";
-    echo "</div>";
-}
+// 구조체 블록을 찾아서 출력
+$file_contents = preg_replace_callback($patterns['struct'], function($matches) {
+    return "<div class='code-block struct' style='background-color: lightcoral;'><h3>Struct: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+}, $file_contents);
+
+// 전체 코드 출력 (구분된 색상으로)
+echo $file_contents;
 
 echo "</div>"; // End of code-container
 
