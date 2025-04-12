4528ba97b264805f8f1f122e2b705a09adf1c221
Son Seung Min <sonsmmm@naver.com>
2025-04-12 14:46:45 +0900

    guideline2 fix

--- judge/src/web/guideline2.php
+++ judge/src/web/guideline2.php
@@ -8,7 +8,7 @@ $file_path = "/home/Capstone_Design_Troy/test/test.txt";
 // 1. 파일 내용 읽기
 $file_contents = file_get_contents($file_path);
 
-// 2. 정규 표현식을 사용하여 각 블록을 색상별로 구분합니다.
+// 2. 정규 표현식을 사용하여 각 블록을 구분합니다.
 $patterns = [
     'func_def' => "/\[func_def_start\((.*?)\)\](.*?)\[func_def_end\((.*?)\)\]/s",
     'rep' => "/\[rep_start\((.*?)\)\](.*?)\[rep_end\((.*?)\)\]/s",
@@ -20,35 +20,37 @@ $patterns = [
 // 3. 코드 내에서 [ ]로 감싸진 부분 제거
 $file_contents = preg_replace("/\[[^\]]*\]/", "", $file_contents); // [ ] 안의 내용 삭제
 
-// 4. 각 블록에 색상별로 출력하기
-echo "<div class='code-container'>";
+// 4. 각 블록을 색상별로 출력하기 전에 정규 표현식을 사용해 부분을 처리합니다.
 
 // 함수 블록을 찾아서 출력
 $file_contents = preg_replace_callback($patterns['func_def'], function($matches) {
-    return "<div class='code-block function' style='background-color: lightblue;'><h3>Function: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+    return "<div class='code-block function' style='background-color: lightblue; padding: 10px; margin: 5px;'><h3>Function: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
 }, $file_contents);
 
 // 반복문 블록을 찾아서 출력
 $file_contents = preg_replace_callback($patterns['rep'], function($matches) {
-    return "<div class='code-block loop' style='background-color: lightpink;'><h3>Loop: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+    return "<div class='code-block loop' style='background-color: lightpink; padding: 10px; margin: 5px;'><h3>Loop: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
 }, $file_contents);
 
 // 조건문 블록을 찾아서 출력
 $file_contents = preg_replace_callback($patterns['cond'], function($matches) {
-    return "<div class='code-block conditional' style='background-color: lightgreen;'><h3>Conditional: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+    return "<div class='code-block conditional' style='background-color: lightgreen; padding: 10px; margin: 5px;'><h3>Conditional: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
 }, $file_contents);
 
 // self-contained 블록을 찾아서 출력
 $file_contents = preg_replace_callback($patterns['self'], function($matches) {
-    return "<div class='code-block self-block' style='background-color: lightyellow;'><h3>Self Block: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+    return "<div class='code-block self-block' style='background-color: lightyellow; padding: 10px; margin: 5px;'><h3>Self Block: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
 }, $file_contents);
 
 // 구조체 블록을 찾아서 출력
 $file_contents = preg_replace_callback($patterns['struct'], function($matches) {
-    return "<div class='code-block struct' style='background-color: lightcoral;'><h3>Struct: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
+    return "<div class='code-block struct' style='background-color: lightcoral; padding: 10px; margin: 5px;'><h3>Struct: {$matches[1]}</h3><p>" . nl2br(htmlspecialchars($matches[2])) . "</p></div>";
 }, $file_contents);
 
 // 전체 코드 출력 (구분된 색상으로)
+echo "<div class='code-container' style='font-family: Arial, sans-serif; line-height: 1.6;'>";
+
+// 전체 코드 출력 (정리된 형태로)
 echo $file_contents;
 
 echo "</div>"; // End of code-container
