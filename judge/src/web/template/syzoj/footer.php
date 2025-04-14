09e63ceebec0d83167e182e7a993985f5350819e
Son Seung Min <sonsmmm@naver.com>
2025-04-15 00:06:28 +0900

    footer change

--- judge/src/web/template/syzoj/footer.php
+++ judge/src/web/template/syzoj/footer.php
@@ -28,15 +28,7 @@
             <div><?php echo $domain==$DOMAIN?$OJ_NAME:ucwords($OJ_NAME)."'s OJ"?> is powered by <a style="color: inherit !important;" class=" " title="GitHub"
                     target="_blank" rel="noreferrer noopener" href="https://github.com/zhblue/hustoj">hustOj</a>, Theme
                 by <a style="color: inherit !important;" href="https://github.com/syzoj">Troy</a></div>
-         <!--   <div> Running on <a href='https://debian.org' target='_blank'>Debian11</a> / <a href='https://www.loongson.cn' target='_blank'>Loongson 3A3000</a> </div> -->
-
-            <?php if ($OJ_BEIAN) { ?>
-            <div>
-            <img src="image/icp.png">
-                <a href="https://beian.miit.gov.cn/" style="text-decoration: none; color: #444444;"
-                    target="_blank"><?php echo $OJ_BEIAN; ?></a>
-            </div>
-            <?php } ?>
+                
         </div>
     </div>
     </div>
