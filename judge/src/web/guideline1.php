caefdb90a7715919c06096c070a6c8463a3a8e35
Son Seung Min <sonsmmm@naver.com>
2025-04-18 22:37:01 +0900

    guideline2.fix

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -65,72 +65,22 @@ function parse_blocks_with_loose_text($text, $depth = 0) {
     return $blocks;
 }
 
-// ✅ 태그 블록 추출
-function extract_tagged_blocks($text) {
-    $pattern = "/\[(func_def|rep|cond|self|struct|construct)_start\\((\d+)\)\]|\[(func_def|rep|cond|self|struct|construct)_end\\((\d+)\)\]/";
-    preg_match_all($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
-
-    $stack = [];
-    $blocks = [];
-
-    foreach ($matches[0] as $match) {
-        $full = $match[0];
-        $pos = $match[1];
-
-        if (strpos($full, '_start(') !== false) {
-            preg_match("/\[(\w+)_start\((\d+)\)\]/", $full, $m);
-            $stack[] = ['type' => $m[1], 'index' => (int)$m[2], 'start' => $pos + strlen($full), 'pos' => $pos];
-        } elseif (strpos($full, '_end(') !== false) {
-            preg_match("/\[(\w+)_end\((\d+)\)\]/", $full, $m);
-            $type = $m[1];
-            $index = (int)$m[2];
-
-            for ($j = count($stack) - 1; $j >= 0; $j--) {
-                if ($stack[$j]['type'] === $type && $stack[$j]['index'] === $index) {
-                    $start = $stack[$j]['start'];
-                    $content = substr($text, $start, $pos - $start);
-                    $blocks[] = ['type' => $type, 'index' => $index, 'content' => trim($content)];
-                    array_splice($stack, $j, 1);
-                    break;
-                }
-            }
-        }
-    }
-
-    usort($blocks, fn($a, $b) => $a['index'] <=> $b['index']);
-    return $blocks;
-}
-
-//라인 태그 마주치면 그 안에 내용 추출(빈 내용의 경우 무시)
 function extract_tagged_code_lines($text) {
-    $pattern = "/\[(func_def|rep|cond|self|struct|construct)_(start|end)\((\d+)\)\]/";
+    $pattern = "/\[(func_def|rep|cond|self|struct|construct)_(start|end)\\((\\d+)\\)\]/";
     preg_match_all($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
 
-    $blocks = [];
     $positions = [];
-
-    // 태그 위치 수집
     foreach ($matches[0] as $i => $match) {
-        $full_tag = $match[0];
-        $pos = $match[1];
-        $type = $matches[1][$i][0];
-        $kind = $matches[2][$i][0];
-        $index = (int)$matches[3][$i][0];
-
         $positions[] = [
-            'type' => $type,
-            'kind' => $kind,
-            'index' => $index,
-            'pos' => $pos,
-            'end' => $pos + strlen($full_tag)
+            'pos' => $match[1],
+            'end' => $match[1] + strlen($match[0])
         ];
     }
 
-    // 태그 간 영역 추출
     $lines = [];
-    for ($i = 0; $i < count($positions) - 1; $i++) {
+    for ($i = 0; $i < count($positions); $i++) {
         $start_pos = $positions[$i]['end'];
-        $end_pos = $positions[$i + 1]['pos'];
+        $end_pos = isset($positions[$i + 1]) ? $positions[$i + 1]['pos'] : strlen($text);
         $code_block = substr($text, $start_pos, $end_pos - $start_pos);
 
         foreach (explode("\n", $code_block) as $line) {
