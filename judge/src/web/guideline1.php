48e0527169379ea866d383d7fe5348deed81f988
Son Seung Min <sonsmmm@naver.com>
2025-04-18 23:21:28 +0900

    guideline2.fix

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -2,14 +2,12 @@
 include("template/syzoj/header.php");
 include("include/db_info.inc.php");
 
-// 설명 텍스트 및 정답 태그 코드 파일
 $file_path = "/home/Capstone_Design_Troy/test/step1_test_tagged_guideline/guideline1.txt";
 $guideline_contents = file_get_contents($file_path);
 
 $txt_path = "/home/Capstone_Design_Troy/test/step1_test_tagged_guideline/tagged_code1.txt";
 $txt_contents = file_get_contents($txt_path);
 
-// 설명 파일 트리 구조 파싱
 function parse_blocks_with_loose_text($text, $depth = 0) {
     $pattern = "/\[(func_def|rep|cond|self|struct|construct)_start\\((\\d+)\\)\](.*?)\[(func_def|rep|cond|self|struct|construct)_end\\(\\2\\)\]/s";
     $blocks = [];
@@ -85,22 +83,17 @@ function extract_tagged_code_lines($text) {
 
         foreach (explode("\n", $code_block) as $line) {
             $trimmed = trim($line);
-            if ($trimmed !== '') {
-                $lines[] = ['content' => $trimmed];
-            }
+            $lines[] = ['content' => $trimmed];
         }
     }
 
     return $lines;
 }
 
-// 환경변수
 $sid = isset($_GET['problem_id']) ? urlencode($_GET['problem_id']) : '';
 $OJ_BLOCK_TREE = parse_blocks_with_loose_text($guideline_contents);
 $OJ_CORRECT_ANSWERS = extract_tagged_code_lines($txt_contents);
 $OJ_SID = $sid;
 
-// 출력
 include("template/$OJ_TEMPLATE/guideline1.php");
 include("template/$OJ_TEMPLATE/footer.php");
-?>
