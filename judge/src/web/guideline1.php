fde99d8ca5cf66e6772ff47d00c18341a6a4ff73
Son Seung Min <sonsmmm@naver.com>
2025-04-19 14:55:10 +0900

    guideline2.fix

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -93,7 +93,7 @@ function extract_tagged_code_lines($text) {
                     $lines[] = [
                         'content' => $trimmed,
                         'readonly' => true,
-                        'info' => '닫는 괄호'};
+                        'info' => '닫는 괄호';
                 } else {
                     $lines[] = ['content' => $trimmed];
                 }
