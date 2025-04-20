50ee1cf4c3556b3c514d73e965fcb1931c011a77
Son Seung Min <sonsmmm@naver.com>
2025-04-21 01:25:06 +0900

    faqs.php fix

--- judge/src/web/template/syzoj/faqs.php
+++ judge/src/web/template/syzoj/faqs.php
@@ -1,35 +1,44 @@
-<?php include("template/$OJ_TEMPLATE/header.php");?>
+<?php include("template/$OJ_TEMPLATE/header.php"); ?>
 
-<?php
-if (!isset($list) || !is_array($list)) {
-    echo "<h2>문제 전략 게시판</h2><p>표시할 전략이 없습니다.</p>";
-    return;
-}
-?>
 <div class="ui container" style="margin-top: 2em">
-    <h2 class="ui header">문제 해결 전략 게시판</h2>
-    <div class="ui divided items">
-        <?php foreach ($list as $item): ?>
-            <div class="item">
-                <div class="content">
-                    <a class="header" href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
-                        <?php echo htmlspecialchars($item['title']) ?>
-                    </a>
-                    <div class="meta">
-                        <span>기초 문제 ID: <?php echo $item['problem_id'] ?></span>
-                    </div>
-                    <div class="description">
-                        <p><?php echo mb_substr(strip_tags($item['description']), 0, 80) ?>...</p>
-                    </div>
-                    <div class="extra">
-                        <a class="ui primary button" href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
-                            전략 보기
-                        </a>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+    <h2 class="ui dividing header">문제 해결 전략 게시판</h2>
+    <?php if (!isset($list) || !is_array($list) || count($list) === 0): ?>
+        <div class="ui warning message">
+            <div class="header">표시할 전략이 없습니다.</div>
+            새로운 전략을 등록하거나, 나중에 다시 확인해 주세요.
+        </div>
+    <?php else: ?>
+        <table class="ui celled table">
+            <thead>
+                <tr>
+                    <th style="width: 10%;">전략 ID</th>
+                    <th style="width: 25%;">제목</th>
+                    <th style="width: 15%;">기초 문제 ID</th>
+                    <th style="width: 40%;">요약 설명</th>
+                    <th style="width: 10%;">보기</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($list as $item): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($item['id']) ?></td>
+                        <td>
+                            <a href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
+                                <?php echo htmlspecialchars($item['title']) ?>
+                            </a>
+                        </td>
+                        <td><?php echo htmlspecialchars($item['problem_id']) ?></td>
+                        <td><?php echo mb_substr(strip_tags($item['description']), 0, 80) ?>...</td>
+                        <td>
+                            <a class="ui mini primary button" href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
+                                전략 보기
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </div>
 
-<?php include("template/$OJ_TEMPLATE/footer.php");
+<?php include("template/$OJ_TEMPLATE/footer.php"); ?>
