a87000f91624cb157de4783c6ecf8011432331e2
Son Seung Min <sonsmmm@naver.com>
2025-04-20 13:14:45 +0900

    problem.php fix

--- judge/src/web/problem.php
+++ judge/src/web/problem.php
@@ -1,188 +1,181 @@
 <?php
+// 캐시 설정
 $cache_time = 10;
 $OJ_CACHE_SHARE = false;
 
+// 공통 include
 require_once('./include/cache_start.php');
 require_once('./include/db_info.inc.php');
-require_once('./include/bbcode.php' );
+require_once('./include/bbcode.php');
 require_once('./include/const.inc.php');
 require_once('./include/my_func.inc.php');
 require_once('./include/setlang.php');
-if(isset($OJ_LANG)){
-	require_once("./lang/$OJ_LANG.php");
-}
+if (isset($OJ_LANG)) require_once("./lang/$OJ_LANG.php");
 
-$now = date("Y-m-d H:i",time());
+// 현재 시간 구하기
+$now = date("Y-m-d H:i");
 
-if (isset($_GET['cid']))
-	$ucid = "&cid=".intval($_GET['cid']);
-else
-	$ucid = "";
+// URL 인자 처리
+$ucid = isset($_GET['cid']) ? "&cid=" . intval($_GET['cid']) : "";
 
-$pr_flag = false;
-$co_flag = false;
+// 문제 출처 플래그 초기화
+$pr_flag = $co_flag = false;
+$id = null;
 
+/////////////////////////////
+// 연습 문제로 접근한 경우 //
+/////////////////////////////
 if (isset($_GET['id'])) {
-	//practice
-	$id = intval($_GET['id']);
-	//require("oj-header.php");
-	
-	$sql="select c.contest_id,c.title from contest c inner join contest_problem cp on c.contest_id=cp.contest_id and cp.problem_id=?  WHERE ( c.`end_time`>'$now' and c.defunct='N' ) or c.`private`='1' ";
-	$used_in_contests=pdo_query($sql,$id);
+    $id = intval($_GET['id']);
 
-	if (isset($_SESSION[$OJ_NAME.'_'.'administrator'])|| isset($_SESSION[$OJ_NAME.'_'.'problem_verifiter'])  || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))
-		$sql = "SELECT * FROM `problem` WHERE `problem_id`=?";   
-	else if ($OJ_FREE_PRACTICE)
-                $sql = "SELECT * FROM `problem` WHERE defunct='N' and `problem_id`=?";
-	else
-		$sql = "SELECT * FROM `problem` WHERE `problem_id`=? AND `defunct`='N' AND `problem_id` NOT IN (
-				SELECT `problem_id` FROM `contest_problem` WHERE `contest_id` IN (
-					SELECT `contest_id` FROM `contest` WHERE ( `end_time`>'$now' and defunct='N' ) or `private`='1'    
-				)
-			)";        //////////  people should not see the problem used in contest before they end by modifying url in browser address bar
-				   /////////   if you give students opportunities to test their result out side the contest ,they can bypass the penalty time of 20 mins for
-	                           /////////   each non-AC sumbission in contest. if you give them opportunities to view problems before exam ,they will ask classmates to write
-	                           /////////   code for them in advance, if you want to share private contest problem to practice you should modify the contest into public
+    // 아직 끝나지 않았거나 private인 contest에서 사용 중인 문제 조회
+    $sql = "SELECT c.contest_id, c.title 
+            FROM contest c 
+            INNER JOIN contest_problem cp ON c.contest_id=cp.contest_id 
+            WHERE cp.problem_id=? AND ((c.end_time>'$now' AND c.defunct='N') OR c.private='1')";
+    $used_in_contests = pdo_query($sql, $id);
 
-	$pr_flag = true;
-	$result = pdo_query($sql,$id);
+    // 관리자/출제자/검토자/편집자는 모든 문제 열람 가능
+    $sql = (isset($_SESSION["{$OJ_NAME}_administrator"]) || isset($_SESSION["{$OJ_NAME}_problem_verifiter"])
+         || isset($_SESSION["{$OJ_NAME}_contest_creator"]) || isset($_SESSION["{$OJ_NAME}_problem_editor"]))
+         ? "SELECT * FROM problem WHERE problem_id=?"
+         : ($OJ_FREE_PRACTICE
+            ? "SELECT * FROM problem WHERE defunct='N' AND problem_id=?"
+            : // 연습 모드가 아니면 아직 끝나지 않았거나 private인 contest에 포함된 문제는 차단
+              "SELECT * FROM problem WHERE problem_id=? AND defunct='N' AND problem_id NOT IN (
+                SELECT problem_id FROM contest_problem 
+                WHERE contest_id IN (
+                    SELECT contest_id FROM contest 
+                    WHERE (end_time>'$now' AND defunct='N') OR private='1'
+                )
+              )");
+
+    $pr_flag = true;
+    $result = pdo_query($sql, $id);
 }
-else if (isset($_GET['cid']) && isset($_GET['pid'])) {
-	//contest
-	$cid = intval($_GET['cid']);
-	$pid = intval($_GET['pid']);
-require_once("contest-check.php");
-	if (isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))
-		$sql = "SELECT langmask,private,defunct FROM `contest` WHERE `contest_id`=?";
-	else
-		$sql = "SELECT langmask,private,defunct FROM `contest` WHERE `defunct`='N' AND `contest_id`=? AND (`start_time`<='$now' AND ('$now'<`end_time` or private='N') )";
 
-	$result = pdo_query($sql,$cid);
-	$rows_cnt =empty($result)?0:count($result);
-	if (empty($result) && !$OJ_FREE_PRACTICE && !isset($_SESSION[$OJ_NAME.'_administrator']) && !isset($_SESSION[$OJ_NAME."_c".$cid]) ) {
-		$view_errors = "<title>$MSG_CONTEST</title><h2>No such Contest!</h2>";
-		require("template/".$OJ_TEMPLATE."/error.php");
-		exit(0);
-	}
+//////////////////////////////////
+// contest 문제로 접근한 경우 //
+//////////////////////////////////
+elseif (isset($_GET['cid']) && isset($_GET['pid'])) {
+    $cid = intval($_GET['cid']);
+    $pid = intval($_GET['pid']);
 
-	$row = ($result[0]);
-	$contest_ok = true;
+    // contest 접근 검사
+    require_once("contest-check.php");
 
-	if ($row[1] && !isset($_SESSION[$OJ_NAME.'_'.'c'.$cid]))
-		$contest_ok = false;
+    // contest 유효성 검사 쿼리
+    $sql = (isset($_SESSION["{$OJ_NAME}_administrator"]) || isset($_SESSION["{$OJ_NAME}_contest_creator"]) || isset($_SESSION["{$OJ_NAME}_problem_editor"]))
+         ? "SELECT langmask, private, defunct FROM contest WHERE contest_id=?"
+         : "SELECT langmask, private, defunct FROM contest 
+            WHERE defunct='N' AND contest_id=? 
+            AND (start_time <= '$now' AND ('$now' < end_time OR private='N'))";
 
-	if ($row[2]=='Y')
-		$contest_ok = false;
+    $result = pdo_query($sql, $cid);
 
-	if (isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))
-		$contest_ok = true;
-	
-	$ok_cnt = $rows_cnt==1;              
-	$langmask = $row[0];
+    // contest가 없거나 접근 권한 없을 때 차단
+    if (empty($result) && !$OJ_FREE_PRACTICE && !isset($_SESSION["{$OJ_NAME}_administrator"]) && !isset($_SESSION["{$OJ_NAME}_c$cid"])) {
+        $view_errors = "<title>$MSG_CONTEST</title><h2>No such Contest!</h2>";
+        require("template/$OJ_TEMPLATE/error.php"); exit;
+    }
 
-	if ( !$contest_ok ) {
-		//not started
-		$view_errors = "No such Contest!";
-		require("template/".$OJ_TEMPLATE."/error.php");
-		exit(0);
-	}
-	else {
-		//started
-//	$sql = "SELECT * FROM `problem` WHERE `defunct`='N' AND `problem_id`=(  // <- defunct problem not in list
-//	$sql = "SELECT * FROM `problem` WHERE `problem_id`=(    // <-- defunct problem in list for contest but, not in list for practice
-		$sql = "SELECT * FROM `problem` WHERE `problem_id`=(
-			SELECT `problem_id` FROM `contest_problem` WHERE `contest_id`=? AND `num`=?
-		)";
-		
-		$result = pdo_query($sql,$cid,$pid);
-		$id = $result[0]['problem_id'];
-	}
+    $row = $result[0];
+    $contest_ok = !(($row[1] && !isset($_SESSION["{$OJ_NAME}_c$cid"])) || $row[2] == 'Y');
 
-	//public
-	if (!$contest_ok) {
-		$view_errors = "Not Invited!";
-		require("template/".$OJ_TEMPLATE."/error.php");
-		exit(0);
-	}
+    // 관리자/출제자 등은 강제 통과
+    if (isset($_SESSION["{$OJ_NAME}_administrator"]) || isset($_SESSION["{$OJ_NAME}_contest_creator"]) || isset($_SESSION["{$OJ_NAME}_problem_editor"])) {
+        $contest_ok = true;
+    }
 
-	$co_flag = true;
+    // 권한 없을 경우 차단
+    if (!$contest_ok) {
+        $view_errors = "No such Contest!";
+        require("template/$OJ_TEMPLATE/error.php"); exit;
+    }
+
+    // contest 내 문제 정보 로드
+    $sql = "SELECT * FROM problem 
+            WHERE problem_id=(SELECT problem_id FROM contest_problem WHERE contest_id=? AND num=?)";
+    $result = pdo_query($sql, $cid, $pid);
+    $id = $result[0]['problem_id'];
+    $co_flag = true;
 }
+
+/////////////////////////////
+// 문제 정보가 아예 없음 //
+/////////////////////////////
 else {
-	$view_errors="<title>$MSG_NO_SUCH_PROBLEM</title><h2>$MSG_NO_SUCH_PROBLEM</h2>";
-	require("template/".$OJ_TEMPLATE."/error.php");
-	exit(0);
+    $view_errors = "<title>$MSG_NO_SUCH_PROBLEM</title><h2>$MSG_NO_SUCH_PROBLEM</h2>";
+    require("template/$OJ_TEMPLATE/error.php");
+    exit;
 }
 
-if (count($result)!=1) {
-	$view_errors = "";
+//////////////////////////////////
+// 문제가 없는 경우 에러 처리 //
+//////////////////////////////////
+if (count($result) != 1) {
+    $view_errors = "";
+    if (isset($_GET['id']) && count($used_in_contests) > 0 && !isset($OJ_EXAM_CONTEST_ID) && !isset($OJ_ON_SITE_CONTEST_ID)) {
+        $view_errors .= "<hr><br>$MSG_PROBLEM_USED_IN:";
+        foreach ($used_in_contests as $contests)
+            $view_errors .= "<a class='label label-warning' href='contest.php?cid={$contests[0]}'>{$contests[1]}</a><br>";
+    } else {
+        $view_title = "<title>$MSG_NO_SUCH_PROBLEM!</title>";
+        $view_errors .= "<h2>$MSG_NO_SUCH_PROBLEM!</h2>";
+    }
 
-	if (isset($_GET['id'])) {
-		$id = intval($_GET['id']);
-
-	      	if(count($used_in_contests)>0){
-
-	      		if (!(isset($OJ_EXAM_CONTEST_ID)||isset($OJ_ON_SITE_CONTEST_ID))) {
-					$view_errors.= "<hr><br>$MSG_PROBLEM_USED_IN:";
-					foreach($used_in_contests as $contests){
-						$view_errors.= "<a class='label label-warning' href='contest.php?cid=". $contests[0]."'>".$contests[1]." </a><br>";	
-					
-					}
-					//echo "</div>";
-			}
-
-		}
-		else {
-			$view_title = "<title>$MSG_NO_SUCH_PROBLEM!</title>";
-			$view_errors .= "<h2>$MSG_NO_SUCH_PROBLEM!</h2>";
-		}
-	}
-	else {
-		$view_title = "<title>$MSG_NO_SUCH_PROBLEM!</title>";
-		$view_errors .= "<h2>$MSG_NO_SUCH_PROBLEM!</h2>";
-	}
-	if(!(isset($_SESSION[$OJ_NAME.'_administrator'])||isset($_SESSION[$OJ_NAME.'_problem_editor']))){
-		require("template/".$OJ_TEMPLATE."/error.php");
-		exit(0);
-	}
-}
+    if (!isset($_SESSION["{$OJ_NAME}_administrator"]) && !isset($_SESSION["{$OJ_NAME}_problem_editor"])) {
+        require("template/$OJ_TEMPLATE/error.php");
+        exit;
+    }
+} 
+//////////////////////////////////
+// 정상적으로 문제 로드한 경우 //
+//////////////////////////////////
 else {
-	$row = $result[0];
-	$view_title = $row['title'];     
+    $row = $result[0];
+    $view_title = $row['title'];
 }
-$flag=false;
-if( isset($OJ_NOIP_KEYWORD) && $OJ_NOIP_KEYWORD){
-        //현재 문제는 NOIP 모드 대회인지 확인
-        //$now =  date('Y-m-d H:i', time());
-        $sql = "select 1 from `contest_problem` where (`problem_id`= ? ) and `contest_id` IN (select `contest_id` from `contest` where `start_time` < ? and `end_time` > ? and `title` like ?)";
-        $rrs = pdo_query($sql, $id ,$now , $now , "%$OJ_NOIP_KEYWORD%");
-        $flag = !empty($rrs) ;
+
+////////////////////////////////////////////////
+// NOIP 모드 문제이거나 문제 잠금 처리된 경우 //
+////////////////////////////////////////////////
+$flag = false;
+if (!empty($OJ_NOIP_KEYWORD)) {
+    $sql = "SELECT 1 FROM contest_problem 
+            WHERE problem_id=? 
+            AND contest_id IN (
+                SELECT contest_id FROM contest 
+                WHERE start_time < ? AND end_time > ? AND title LIKE ?
+            )";
+    $flag = !empty(pdo_query($sql, $id, $now, $now, "%$OJ_NOIP_KEYWORD%"));
 }
-        if($flag||problem_locked($id,28)){
-                $row[ 'accepted' ] = '<font color="red"> ? </font>';
-                $row[ 'submit' ] = '<font color="red"> ? </font>';
 
-                // NOIP 모드 대회가 아니라면, AC 수치를 표시합니다.
-                if (isset($OJ_NOIP_HINT) && $OJ_NOIP_HINT) {
-                    //$row['hint'] = $MSG_NOIP_NOHINT;
-                } else {
-                    $row['hint'] = $MSG_NOIP_NOHINT;
-                }
-        }
+if ($flag || problem_locked($id, 28)) {
+    $row['accepted'] = $row['submit'] = "<font color='red'> ? </font>";
+    if (isset($OJ_NOIP_HINT) && $OJ_NOIP_HINT) {
+        $row['hint'] = $MSG_NOIP_NOHINT;
+    }
+}
 
-	       $solution_file = "$OJ_DATA/$id/output.name";
+///////////////////////////////////////////
+// 문제 출력 예시 파일 있는 경우 표시 //
+///////////////////////////////////////////
+$solution_file = "$OJ_DATA/$id/output.name";
+if (file_exists($solution_file)) {
+    $content = file_get_contents($solution_file);
+    $filename = pathinfo($content, PATHINFO_FILENAME);
+}
 
-	 if (file_exists($solution_file)) {
-        // 파일 내용을 읽다
-        $content = file_get_contents($solution_file);
-        
-        // 파일 이름 부분 추출 (확장자 제거)
-        $filename = pathinfo($content, PATHINFO_FILENAME);
-        
-    } 
-//if($row['spj']<=1) $row['description']=aaiw($row['description']);
-/////////////////////////Template
-require("template/".$OJ_TEMPLATE."/problem.php");
-/////////////////////////Common foot
-if(file_exists('./include/cache_end.php'))
-	require_once('./include/cache_end.php');
+//////////////////////////
+// 문제 화면 템플릿 로드 //
+//////////////////////////
+require("template/$OJ_TEMPLATE/problem.php");
+
+/////////////////////
+// 캐시 종료 처리 //
+/////////////////////
+if (file_exists('./include/cache_end.php')) {
+    require_once('./include/cache_end.php');
+}
 ?>
