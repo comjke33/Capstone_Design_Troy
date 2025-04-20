fdec5b76a5490ba964be38e3ba470874983d0916
Son Seung Min <sonsmmm@naver.com>
2025-04-21 01:59:27 +0900

    faqs.php fix

--- judge/src/web/template/syzoj/faqs.php
+++ judge/src/web/template/syzoj/faqs.php
@@ -1,7 +1,7 @@
 <?php include("template/$OJ_TEMPLATE/header.php"); ?>
 
-<div class="ui container" style="margin-top: 2em">
-    <h2 class="ui header" style="font-weight: 500;">문제 해결 전략 게시판</h2>
+<div class="ui container" style="margin-top: 3em; margin-bottom: 4em;">
+    <h2 class="ui dividing header">문제 해결 전략 게시판</h2>
 
     <?php
     require_once("./include/db_info.inc.php");
@@ -15,44 +15,42 @@
             <p>표시할 문제 해결 전략이 없습니다.</p>
         </div>
     <?php else: ?>
-        <table class="ui very basic table" style="font-size: 0.95em;">
+        <table class="ui celled table" style="font-size: 0.95em;">
             <thead>
-                <tr>
+                <tr class="center aligned">
                     <th style="width: 18%;">전략 제목</th>
                     <th style="width: 10%;">문제 ID</th>
                     <th style="width: 22%;">문제 설명</th>
                     <th style="width: 20%;">보조 함수</th>
                     <th style="width: 20%;">예제 코드</th>
-                    <th style="width: 10%; text-align: center;">보기</th>
+                    <th style="width: 10%;">문제 이동</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($list as $item): ?>
                     <tr>
                         <td>
-                            <a <?php echo $item['id'] ?>">
-                                <?php echo htmlspecialchars($item['title']) ?>
-                            </a>
+                            <strong><?php echo htmlspecialchars(mb_strimwidth($item['title'], 0, 25, '...')) ?></strong>
                         </td>
-                        <td><?php echo intval($item['problem_id']) ?></td>
-                        <td><?php echo mb_substr(strip_tags($item['description']), 0, 50) ?>...</td>
+                        <td class="center aligned"><?php echo intval($item['problem_id']) ?></td>
+                        <td><?php echo htmlspecialchars(mb_strimwidth(strip_tags($item['description']), 0, 50, '...')) ?></td>
                         <td>
-                            <?php
-                            if (!empty($item['helper_function'])) {
-                                echo nl2br(htmlspecialchars(trim($item['helper_function'])));
-                            } else {
-                                echo '-';
-                            }
-                            ?>
-                        </td>
-                        <td>
-                            <div style="max-height: 120px; overflow-y: auto; background: #f9f9f9; padding: 0.5em; border-radius: 4px;">
-                                <code style="white-space: pre-wrap;"><?php echo htmlspecialchars(mb_substr($item['title'], 0, 50)) ?>...</code>
+                            <div style="white-space: pre-wrap;">
+                                <?php
+                                echo !empty(trim($item['helper_function']))
+                                    ? nl2br(htmlspecialchars(trim($item['helper_function'])))
+                                    : '-';
+                                ?>
                             </div>
                         </td>
-                            <td style="text-align: center;">
-                            <a class="ui mini basic button" href="problem.php?id=<?php echo intval($item['problem_id']) ?>">
-                                문제 이동
+                        <td>
+                            <div style="max-height: 100px; overflow-y: auto; background: #f4f4f4; padding: 0.7em; border-radius: 6px; font-family: monospace;">
+                                <code><?php echo htmlspecialchars(mb_strimwidth($item['solution_code'], 0, 200, '...')) ?></code>
+                            </div>
+                        </td>
+                        <td class="center aligned">
+                            <a class="ui mini blue basic button" href="problem.php?id=<?php echo intval($item['problem_id']) ?>">
+                                보기
                             </a>
                         </td>
                     </tr>
