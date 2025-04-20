61d090740c8bea8762c9396912abe14bc2198262
Son Seung Min <sonsmmm@naver.com>
2025-04-20 20:37:50 +0900

    fix submitpage

--- judge/src/web/template/syzoj/submitpage.php
+++ judge/src/web/template/syzoj/submitpage.php
@@ -67,6 +67,7 @@ for ($i = 0; $i < $lang_count; $i++) {
 <input name="vcode" size=4 type=text autocomplete=off ><img id="vcode" alt="click to change" src="vcode.php" onclick="this.src='vcode.php?'+Math.random()">
 <?php } ?>
 
+<!-- 제출 처리 -->
 <button id="Submit" type="button" class="ui primary icon button" onclick="do_submit();"><?php echo $MSG_SUBMIT?></button>
 <label id="countDown"></label>
 <?php if (isset($OJ_ENCODE_SUBMIT)&&$OJ_ENCODE_SUBMIT){?>
@@ -74,81 +75,41 @@ for ($i = 0; $i < $lang_count; $i++) {
 <input type=hidden id="encoded_submit_mark" name="reverse2" value="reverse"/>
 <?php }?>
 
+<!-- 테스트 실행 -->
 <?php if ($spj>1 || !$OJ_TEST_RUN ){?>
-<span class="btn" id=result><?php echo $MSG_STATUS?></span>	
+<span class="btn" id=result><?php echo $MSG_STATUS?></span> 
 <?php }?>
 </span>
 
 <?php 
 if(!$solution_name){
     if($OJ_ACE_EDITOR){
-        $height = isset($OJ_TEST_RUN) && $OJ_TEST_RUN ? "400px" : "500px";
-        echo "<pre style=\"width:90%;height:$height\" cols=180 rows=16 id=\"source\">" . htmlentities($view_src, ENT_QUOTES, "UTF-8") . "</pre>";
-        echo "<input type=hidden id=\"hide_source\" name=\"source\" value=\"\"/>";
-    } else {
-        echo "<textarea style=\"width:80%;height:600px\" cols=180 rows=30 id=\"source\" name=\"source\">" . htmlentities($view_src, ENT_QUOTES, "UTF-8") . "</textarea>";
+        if (isset($OJ_TEST_RUN) && $OJ_TEST_RUN)
+            $height = "400px";
+        else
+            $height = "500px";
+    ?>
+        <pre style="width:90%;height:<?php echo $height ?>" cols=180 rows=16 id="source"><?php echo htmlentities($view_src, ENT_QUOTES, "UTF-8") ?></pre>
+        <input type="hidden" id="hide_source" name="source" value=""/>
+    <?php 
+    } else { ?>
+        <textarea style="width:80%;height:600px" cols=180 rows=30 id="source" name="source"><?php echo htmlentities($view_src, ENT_QUOTES, "UTF-8") ?></textarea>
+    <?php 
     }
 } else {
     echo "<br><h2>제출할 파일로 지정된 파일명: $solution_name</h2>";
 }
 ?>
 
-<?php if (isset($OJ_TEST_RUN) && $OJ_TEST_RUN && $spj <= 1 && !$solution_name) { ?>
-<div class="row">
-    <div class="column" style="display: flex;">
-        <div style="margin-left: 60px; width: 40%; padding: 14px; flex-direction: column;">
-            <div style="display: flex; border-radius: 8px; background-color: rgba(255,255,255,0.4);" id="language_span"><?php echo $MSG_Input?></div>
-            <textarea style="width:100%" cols=40 rows=5 id="input_text" name="input_text" ><?php echo $view_sample_input?></textarea>
-        </div>
-        <div style="width: 40%; flex-direction: column;">
-            <div style="display: flex; border-radius: 8px; background-color: rgba(255,255,255,0.4); justify-content: space-between;" id="language_span">
-                <?php echo $MSG_Output ?>
-                <span class="btn" id=result><?php echo $MSG_STATUS?></span>
-            </div>
-            <textarea style="width:100%; background-color: white;" cols=10 rows=5 id="out" name="out" disabled="true" placeholder='<?php echo htmlentities($view_sample_output, ENT_QUOTES, 'UTF-8') ?>'></textarea>
-        </div>
-    </div>
-    <input style="margin-top: 30px; margin-left: 0 auto; width: 7%; background-color: #22ba46a3; border-color: #00fff470; height: 130px;" id="TestRun" class="btn btn-info" type=button value="<?php echo $MSG_TR?>" onclick=do_test_run();>
+<!-- 문제 입력 -->
+<div style="margin-top:10px;">
+<label for="problem_id_manual">Problem ID (optional):</label>
+<input type="text" id="problem_id_manual" name="problem_id_manual" placeholder="If not in contest mode">
 </div>
-<?php } ?>
 
-<?php if (isset($OJ_BLOCKLY)&&$OJ_BLOCKLY){?>
-<input id="blockly_loader" type=button class="btn" onclick="openBlockly()" value="<?php echo $MSG_BLOCKLY_OPEN?>" style="color:white;background-color:rgb(169,91,128)">
-<input id="transrun" type=button class="btn" onclick="loadFromBlockly() " value="<?php echo $MSG_BLOCKLY_TEST?>" style="display:none;color:white;background-color:rgb(90,164,139)">
-<div id="blockly" class="center">
-    <iframe name='frmBlockly' width=90% height=580 src='blockly/demos/code/index.html'></iframe>
-</div>
-<?php } ?>
-
-<?php if (!empty($OJ_REMOTE_JUDGE)) { ?>
+<?php if (!empty($OJ_REMOTE_JUDGE)){?>
 <iframe src="remote.php" height="0px" width="0px"></iframe>
-<?php } ?>
+<?php }?>
+
 </form>
 </center>
-
-<script src="<?php echo $OJ_CDN_URL?>include/base64.js"></script>
-<?php if($OJ_ACE_EDITOR){ ?>
-<script src="<?php echo $OJ_CDN_URL?>ace/ace.js"></script>
-<script src="<?php echo $OJ_CDN_URL?>ace/ext-language_tools.js"></script>
-<script>
-ace.require("ace/ext/language_tools");
-var editor = ace.edit("source");
-editor.setTheme("ace/theme/xcode");
-switchLang(<?php echo $lastlang ?>);
-editor.setOptions({
-    enableBasicAutocompletion: true,
-    enableSnippets: true,
-    enableLiveAutocompletion: true,
-    fontSize: "18px"
-});
-
-editor.getSession().on("change", function() {
-    var markers = editor.getSession().getMarkers(false);
-    for (var id in markers) {
-        if (markers[id].clazz === "ace_error_marker") {
-            editor.getSession().removeMarker(id);
-        }
-    }
-});
-</script>
-<?php } ?>
