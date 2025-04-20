de6bfa0b1f6ee464c576fac2a30ffe0a67a98329
Son Seung Min <sonsmmm@naver.com>
2025-04-21 01:27:05 +0900

    faqs.php fix

--- judge/src/web/template/syzoj/faqs.php
+++ judge/src/web/template/syzoj/faqs.php
@@ -1,36 +1,48 @@
-<?php include("template/$OJ_TEMPLATE/header.php"); ?>
+<?php include("template/$OJ_TEMPLATE/header.php");?>
 
 <div class="ui container" style="margin-top: 2em">
     <h2 class="ui dividing header">문제 해결 전략 게시판</h2>
+
     <?php if (!isset($list) || !is_array($list) || count($list) === 0): ?>
         <div class="ui warning message">
-            <div class="header">표시할 전략이 없습니다.</div>
-            새로운 전략을 등록하거나, 나중에 다시 확인해 주세요.
+            <div class="header">전략 없음</div>
+            <p>표시할 문제 해결 전략이 없습니다.</p>
         </div>
     <?php else: ?>
-        <table class="ui celled table">
+        <table class="ui celled striped table">
             <thead>
                 <tr>
-                    <th style="width: 10%;">전략 ID</th>
-                    <th style="width: 25%;">제목</th>
-                    <th style="width: 15%;">기초 문제 ID</th>
-                    <th style="width: 40%;">요약 설명</th>
-                    <th style="width: 10%;">보기</th>
+                    <th>전략 제목</th>
+                    <th>기초 문제 ID</th>
+                    <th>문제 설명</th>
+                    <th>보조 함수 목록</th>
+                    <th>예제 코드</th>
+                    <th>자세히 보기</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($list as $item): ?>
                     <tr>
-                        <td><?php echo htmlspecialchars($item['id']) ?></td>
                         <td>
                             <a href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
                                 <?php echo htmlspecialchars($item['title']) ?>
                             </a>
                         </td>
-                        <td><?php echo htmlspecialchars($item['problem_id']) ?></td>
-                        <td><?php echo mb_substr(strip_tags($item['description']), 0, 80) ?>...</td>
+                        <td><?php echo $item['problem_id'] ?></td>
+                        <td><?php echo mb_substr(strip_tags($item['description']), 0, 50) ?>...</td>
                         <td>
-                            <a class="ui mini primary button" href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
+                            <ul>
+                                <?php foreach (explode(',', $item['helper_functions']) as $func): ?>
+                                    <li><?php echo htmlspecialchars(trim($func)) ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </td>
+                        <td>
+                            <pre style="overflow-x: auto; max-width: 300px; font-size: 0.9em;">
+<?php echo htmlspecialchars(mb_substr(strip_tags($item['example_code']), 0, 200)) ?>...</pre>
+                        </td>
+                        <td>
+                            <a class="ui primary mini button" href="faqs.php?action=detail&id=<?php echo $item['id'] ?>">
                                 전략 보기
                             </a>
                         </td>
