79ad1bfe1df678f87e5924a9fe345230df9a918d
Son Seung Min <sonsmmm@naver.com>
2025-04-21 02:47:19 +0900

    similar_problem +

--- judge/src/web/status.php
+++ judge/src/web/status.php
@@ -514,7 +514,7 @@ for ($i=0; $i<$rows_cnt; $i++) {
       if ($row['result'] != 4) {  // Accepted가 아닌 경우
           $view_status[$i][10] = "<a target=\"_self\" href=\"feedback.php?solution_id={$sid}\" class=\"ui orange mini button\">피드백 보기</a>";
       } else { // Accepted인 경우
-          $view_status[$i][10] = "<a target=\"_self\" href=\"similarproblem.php?solution_id={$sid}\" class=\"ui blue mini button\">유사문제 추천</a>";
+          $view_status[$i][10] = "<a target=\"_self\" href=\"similar_problem.php?solution_id={$sid}\" class=\"ui blue mini button\">유사문제 추천</a>";
       }
   } else {
       $view_status[$i][10] = "-"; // 대회 문제인 경우 출력 안 함
