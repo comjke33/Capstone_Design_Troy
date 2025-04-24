dafef44a24b6fa2f360c6c06f0dc07b66175cce0
Son Seung Min <sonsmmm@naver.com>
2025-04-25 01:01:45 +0900

    background fix

--- judge/src/web/template/sidebar/header.php
+++ judge/src/web/template/sidebar/header.php
@@ -144,126 +144,44 @@
 
 { ?>
    
-<body id="MainBg-C" style="position: relative;background-size: 100%">
-    <div id="page-header" class="ui fixed borderless tiny thin inverted vertical  menu" style="position: fixed; height: 100%; z-index:99999">
+   <body id="MainBg-C" style="position: relative; background-size: 100%">
+    <div id="page-header" class="ui fixed borderless tiny thin inverted vertical menu" style="position: fixed; height: 100%; z-index: 99999">
         <div id="menu" class="ui stackable mobile ui container computer" style="margin-left:auto;margin-right:auto;">
-            <a class="header item"  href="/"><span
-                    style="font-family: 'Exo 2'; font-size: 1.5em; font-weight: 600; "><?php echo $domain==$DOMAIN?$OJ_NAME:ucwords($OJ_NAME)."'s OJ"?></span></a>
-            
-          <?php
-            if(isset($OJ_AI_HTML)&&$OJ_AI_HTML) echo $OJ_AI_HTML;
-            else echo '<a class="desktop-only item" href="/"><i class="home icon"></i>'.$MSG_HOME.'</a>';
-            if(file_exists("moodle"))  // 如果存在moodle目录，自动添加链接
-            {
-              echo '<a class="item" href="moodle"><i class="group icon"></i>Moodle</a>';
-            }
-            if(!isset($_GET['cid'])||$cid==0){
-          ?>
+            <a class="header item" href="/"><span style="font-family: 'Exo 2'; font-size: 1.5em; font-weight: 600; "><?php echo $domain == $DOMAIN ? $OJ_NAME : ucwords($OJ_NAME) . "'s OJ" ?></span></a>
 
-            <a class="item <?php if ($url=="problemset.php") echo "active";?>"
-                href="<?php echo $path_fix?>problemset.php"><i class="list icon"></i><?php echo $MSG_PROBLEMS?> </a>
-            <a class="item <?php if ($url=="category.php") echo "active";?>"
-                href="<?php echo $path_fix?>category.php"><i class="globe icon"></i><?php echo $MSG_SOURCE?></a>
-            <a class="item <?php if ($url=="contest.php") echo "active";?>" href="<?php echo $path_fix?>contest.php<?php if(isset($_SESSION[$OJ_NAME."_user_id"])) echo "?my" ?>" ><i
-                    class="trophy icon"></i> <?php echo $MSG_CONTEST?></a>
-            <a class="item <?php if ($url=="status.php") echo "active";?>" href="<?php echo $path_fix?>status.php"><i
-                    class="tasks icon"></i><?php echo $MSG_STATUS?></a>
-            <a class="desktop-only item <?php if ($url=="ranklist.php") echo "active";?> "
-                href="<?php echo $path_fix?>ranklist.php"><i class="signal icon"></i> <?php echo $MSG_RANKLIST?></a>
-            <!--<a class="item <?php //if ($url=="contest.php") echo "active";?>" href="/discussion/global"><i class="comments icon"></i> 讨论</a>-->
-                
-<?php if(isset($OJ_RECENT_CONTEST)&&$OJ_RECENT_CONTEST){    ?>
-            <a class="desktop-only item <?php if ($url=="recent-contest.php") echo "active";?> "
-                href="<?php echo $path_fix?>recent-contest.php"><i class="bullhorn icon"></i> <?php echo $MSG_RECENT_CONTEST?></a>
-<?php } ?>
+            <!-- 메뉴 항목들 -->
+            <a class="item" href="<?php echo $path_fix ?>problemset.php"><i class="list icon"></i><?php echo $MSG_PROBLEMS ?> </a>
+            <a class="item" href="<?php echo $path_fix ?>category.php"><i class="globe icon"></i><?php echo $MSG_SOURCE ?></a>
+            <a class="item" href="<?php echo $path_fix ?>contest.php"><i class="trophy icon"></i> <?php echo $MSG_CONTEST ?></a>
 
-            <a class="desktop-only item <?php if ($url=="faqs.php") echo "active";?>" href="<?php echo $path_fix?>faqs.php"><i
-                    class="help circle icon"></i> <?php echo $MSG_FAQ?></a>
-
-              <?php if (isset($OJ_BBS)&& $OJ_BBS){ ?>
-                  <a class='item' href="discuss.php"><i class="clipboard icon"></i> <?php echo $MSG_BBS?></a>
-              <?php }
-
-            }
-                ?>
-            <?php if( isset($_GET['cid']) && intval($_GET['cid'])>0 ){
-                $cid=intval($_GET['cid']);
-            ?>
-            <a id="" class="item" href="<?php echo $path_fix?>contest.php" ><i class="arrow left icon"></i><?php echo $MSG_CONTEST.$MSG_LIST?></a>
-            <a id="" class="item active" href="<?php echo $path_fix?>contest.php?cid=<?php echo $cid?>" ><i class="list icon"></i><?php echo $MSG_PROBLEMS.$MSG_LIST?></a>
-            <a id="" class="item active" href="<?php echo $path_fix?>status.php?cid=<?php echo $cid?>" ><i class="tasks icon"></i><?php echo $MSG_STATUS.$MSG_LIST?></a>
-            <a id="" class="item active" href="<?php echo $path_fix?>contestrank.php?cid=<?php echo $cid?>" ><i class="numbered list icon"></i><?php echo $MSG_RANKLIST?></a>
-            <a id="" class="item active" href="<?php echo $path_fix?>contestrank-oi.php?cid=<?php echo $cid?>" ><i class="child icon"></i>OI-<?php echo $MSG_RANKLIST?></a>
-                    <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])){ ?>
-                            <a id="" class="item active" href="<?php echo $path_fix?>conteststatistics.php?cid=<?php echo $cid?>" ><i class="eye icon"></i><?php echo $MSG_STATISTICS?></a>
-                    <?php }  ?>
-            <?php }  ?>
             <?php echo $sql_news_menu_result_html; ?>
+
             <div class="right menu">
-                <?php if(isset($_SESSION[$OJ_NAME.'_'.'user_id'])) { ?>
-                <a href="<?php echo $path_fix?>/userinfo.php?user=<?php echo $_SESSION[$OJ_NAME.'_'.'user_id']?>"
-                    style="color: inherit; ">
+                <?php if (isset($_SESSION[$OJ_NAME . '_' . 'user_id'])) { ?>
+                <a href="<?php echo $path_fix ?>/userinfo.php?user=<?php echo $_SESSION[$OJ_NAME . '_' . 'user_id'] ?>" style="color: inherit;">
                     <div class="ui simple dropdown item">
-                        <?php echo $_SESSION[$OJ_NAME.'_'.'user_id']; 
-                              if(!empty($_SESSION[$OJ_NAME.'_nick'])) echo "(".$_SESSION[$OJ_NAME.'_nick'].")";
-                              if(!empty($_SESSION[$OJ_NAME.'_group_name'])) echo "[".$_SESSION[$OJ_NAME.'_group_name']."]";
-                                      
-                        ?>
+                        <?php echo $_SESSION[$OJ_NAME . '_' . 'user_id']; ?>
                         <i class="dropdown icon"></i>
                         <div class="menu">
-                            <a class="item" href="modifypage.php"><i class="edit icon"></i><?php echo $MSG_REG_INFO;?></a>
-                            <a class="item" href="portal.php"><i class="tasks icon"></i><?php echo $MSG_TODO;?></a>
-                                <?php if ($OJ_SaaS_ENABLE){ ?>
-                                <?php if($_SERVER['HTTP_HOST']==$DOMAIN)
-                                        echo  "<a class='item' href='http://".  $_SESSION[$OJ_NAME.'_'.'user_id'].".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
-                                <?php } ?>
-                            <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])){ ?>
-                            <a class="item" href="admin/"><i class="settings icon"></i><?php echo $MSG_ADMIN;?></a>
-                            <?php }
-if(isset($_SESSION[$OJ_NAME.'_'.'balloon'])){
-  echo "<a class=item href='balloon.php'><i class='golf ball icon'></i>$MSG_BALLOON</a>";
-}
-                              if((isset($OJ_EXAM_CONTEST_ID)&&$OJ_EXAM_CONTEST_ID>0)||
-                                     (isset($OJ_ON_SITE_CONTEST_ID)&&$OJ_ON_SITE_CONTEST_ID>0)||
-                                     (isset($OJ_MAIL)&&!$OJ_MAIL)){
-                                      // mail can not use in contest or mail is turned off
-                              }else{
-                                    $mail=checkmail();
-                                    if($mail) echo "<a class='item mail' href=".$path_fix."mail.php><i class='mail icon'></i>$MSG_MAIL$mail</a>";
-                              }
-
-
-
-
-                            ?>
-        <?php
-        if(isset($OJ_PRINTER) && $OJ_PRINTER)
-        {
-        ?>
-          <a  class="item"  href="printer.php">
-            <i class="print icon"></i> <?php echo $MSG_PRINTER?>
-          </a>
-        <?php
-        }
-        ?>
-                            <a class="item" href="logout.php"><i class="power icon"></i><?php echo $MSG_LOGOUT;?></a>
+                            <a class="item" href="modifypage.php"><i class="edit icon"></i><?php echo $MSG_REG_INFO; ?></a>
+                            <a class="item" href="portal.php"><i class="tasks icon"></i><?php echo $MSG_TODO; ?></a>
                         </div>
                     </div>
                 </a>
                 <?php } else { ?>
-
-
                 <div class="item">
-                    <a class="ui button" style="margin-right: 0.5em; " href="loginpage.php">
-                       <?php echo $MSG_LOGIN?>
-                    </a>
-                    <?php if(isset($OJ_REGISTER)&&$OJ_REGISTER ){ ?>
-                    <a class="ui primary button" href="registerpage.php">
-                       <?php echo $MSG_REGISTER?>
-                    </a>
+                    <a class="ui button" href="loginpage.php"><?php echo $MSG_LOGIN ?></a>
+                    <?php if (isset($OJ_REGISTER) && $OJ_REGISTER) { ?>
+                    <a class="ui primary button" href="registerpage.php"><?php echo $MSG_REGISTER ?></a>
                     <?php } ?>
                 </div>
                 <?php } ?>
+            </div>
+        </div>
+    </div>
+    <div style="margin-left: 168px;">
+        <div id="main" class="ui main container">
+    <?php } ?>
             </div>
      <div class="footer" style="position:absolute;bottom:0;left:-10px">
         <div class="ui center aligned container" title="如果你想移除这个信息，请编辑template/siderbar/footer.php" >
