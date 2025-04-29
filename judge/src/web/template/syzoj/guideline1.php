65696a9ea89926e3f23028ab1136ebaf10251006
Son Seung Min <sonsmmm@naver.com>
2025-04-30 03:29:30 +0900

    guideline.php 들여쓰기 추가

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -10,62 +10,62 @@
     <!-- 왼쪽 패널: 문제 설명과 텍스트 입력 영역 -->
     <div class="left-panel" style="flex: 1; padding-right: 10px;">
         <?php
-            function render_tree_plain($blocks, &$answer_index = 0) {
-                $html = "";
+        function render_tree_plain($blocks, &$answer_index = 0) {
+            $html = "";
 
-                foreach ($blocks as $block) {
-                    $indent_px = 10 * ($block['depth'] ?? 0);
+            foreach ($blocks as $block) {
+                $indent_px = 10 * ($block['depth'] ?? 0);
 
-                    if (isset($block['children'])) {
-                        $html .= "<div class='block-wrap block-{$block['type']}' style='margin-left: {$indent_px}px;'>";
-                        $html .= render_tree_plain($block['children'], $answer_index);
-                        $html .= "</div>";
-                    } elseif ($block['type'] === 'text') {
-                        $raw = trim($block['content']);
+                if (isset($block['children'])) {
+                    $html .= "<div class='block-wrap block-{$block['type']}' style='margin-left: {$indent_px}px;'>";
+                    $html .= render_tree_plain($block['children'], $answer_index);
+                    $html .= "</div>";
+                } elseif ($block['type'] === 'text') {
+                    $raw = trim($block['content']);
 
-                        // 태그라인 무시
-                        if ($raw === '' || preg_match("/^\[(func_def|rep|cond|self|struct|construct)_(start|end)\(\d+\)\]$/", $raw)) {
-                            continue;
-                        }
-
-                        $line = htmlspecialchars($block['content']);
-                        if (strpos($line, '[start]') !== false && strpos($line, '[end]') !== false) {
-                            $line = preg_replace('/\[(.*?)\]/', '', $line);  // 태그 제거
-                            $line = trim($line);
-                        }
-
-                        // 정답 코드가 존재하는지 먼저 확인
-                        $has_correct_answer = isset($GLOBALS['OJ_CORRECT_ANSWERS'][$answer_index]);
-                        $disabled = $has_correct_answer ? "" : "disabled";
-
-                        // 가이드라인 설명 및 코드 입력 영역
-                        $html .= "<div class='submission-line' style='padding-left: {$indent_px}px;'>";
-                        $html .= "<div style='flex: 1'>";
-                        $html .= "<div class='code-line'>{$line}</div>";
-                        $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' {$disabled}></textarea>";
-                        if ($has_correct_answer) {
-                            $html .= "<button onclick='submitAnswer({$answer_index})' id='btn_{$answer_index}' class='submit-button'>제출</button>";
-                            $html .= "<button onclick='showAnswer({$answer_index})' id='view_btn_{$answer_index}' class='view-button'>답안 확인</button>";
-                        }
-                        // 정답이 표시될 공간 추가 (textarea와 제출 버튼 사이)
-                        $html .= "<div id='answer_area_{$answer_index}' class='answer-area' style='display:none; margin-top: 10px;'></div>";
-                        $html .= "</div><div style='width: 50px; text-align: center; margin-top: 20px;'>";
-                        $html .= "<span id='check_{$answer_index}' class='checkmark' style='display:none;'>✔️</span>";
-                        $html .= "</div></div>";
-
-                        $answer_index++;
+                    // 태그라인 무시
+                    if ($raw === '' || preg_match("/^\[(func_def|rep|cond|self|struct|construct)_(start|end)\(\d+\)\]$/", $raw)) {
+                        continue;
                     }
-                }
 
-                return $html;
+                    $line = htmlspecialchars($block['content']);
+                    if (strpos($line, '[start]') !== false && strpos($line, '[end]') !== false) {
+                        $line = preg_replace('/\[(.*?)\]/', '', $line);  // 태그 제거
+                        $line = trim($line);
+                    }
+
+                    // 정답 코드가 존재하는지 먼저 확인
+                    $has_correct_answer = isset($GLOBALS['OJ_CORRECT_ANSWERS'][$answer_index]);
+                    $disabled = $has_correct_answer ? "" : "disabled";
+
+                    // 가이드라인 설명 및 코드 입력 영역
+                    $html .= "<div class='submission-line' style='padding-left: {$indent_px}px;'>";
+                    $html .= "<div style='flex: 1'>";
+                    $html .= "<div class='code-line'>{$line}</div>";
+                    $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' {$disabled}></textarea>";
+                    if ($has_correct_answer) {
+                        $html .= "<button onclick='submitAnswer({$answer_index})' id='btn_{$answer_index}' class='submit-button'>제출</button>";
+                        $html .= "<button onclick='showAnswer({$answer_index})' id='view_btn_{$answer_index}' class='view-button'>답안 확인</button>";
+                    }
+                    // 정답이 표시될 공간 추가 (textarea와 제출 버튼 사이)
+                    $html .= "<div id='answer_area_{$answer_index}' class='answer-area' style='display:none; margin-top: 10px;'></div>";
+                    $html .= "</div><div style='width: 50px; text-align: center; margin-top: 20px;'>";
+                    $html .= "<span id='check_{$answer_index}' class='checkmark' style='display:none;'>✔️</span>";
+                    $html .= "</div></div>";
+
+                    $answer_index++;
+                }
             }
 
-            // 주어진 코드를 파싱하여 문제와 설명을 출력
-            $answer_index = 0;
-            echo render_tree_plain($OJ_BLOCK_TREE, $answer_index);
+            return $html;
+        }
+
+        // 주어진 코드를 파싱하여 문제와 설명을 출력
+        $answer_index = 0;
+        echo render_tree_plain($OJ_BLOCK_TREE, $answer_index);
         ?>
     </div>
-    <div class>
+    <div class="">
 
     </div>
 </div>
