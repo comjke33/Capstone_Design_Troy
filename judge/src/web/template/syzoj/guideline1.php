b4c075e6b7436819a0912303cd0d63fa7beb6fdb
Son Seung Min <sonsmmm@naver.com>
2025-04-30 02:57:41 +0900

    guideline.js seperate

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -73,3 +73,15 @@
 
 <!-- js 불러오기 -->
 <script src="/template/syzoj/js/guideline.js"></script>
+
+<script>
+    // js 파일 로딩이 정상적으로 되었는지 확인하는 코드
+    window.onload = function() {
+        // guideline.js가 정상적으로 로드되었는지 확인
+        if (typeof submitAnswer === 'function' && typeof showAnswer === 'function') {
+            console.log("guideline.js 파일이 정상적으로 로드되었습니다.");
+        } else {
+            console.error("guideline.js 파일 로드에 실패했습니다.");
+        }
+    };
+</script>
