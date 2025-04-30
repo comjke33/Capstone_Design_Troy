f31788217194a4850b0bc691cb776ba9e23c107e
Choi Yeon Ho <cyh990218@naver.com>
2025-05-01 05:16:41 +0900

    파이썬 실행 테스트9

--- judge/src/web/admin/problem_add.php
+++ judge/src/web/admin/problem_add.php
@@ -131,7 +131,7 @@ if(isset($_POST['remote_oj'])){
 
 //파이썬 실행
 $cmd = "python3 /home/Capstone_Design_Troy/button_test/button_test.py";
-exec("cd /home/Capstone_Design_Troy/button_test/ && python3 button_test.py");
+shell_exec("cd /home/Capstone_Design_Troy/button_test/ && python3 button_test.py");
 //파이썬 실행
 
 $sql = "INSERT INTO `privilege` (`user_id`,`rightstr`) VALUES(?,?)";
