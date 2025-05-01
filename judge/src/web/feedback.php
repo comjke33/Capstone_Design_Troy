806a7290a3a2b220fc96f43526c19214301f3395
comjke33 <comjke33@inu.ac.kr>
2025-05-01 14:26:23 +0900

    test

--- judge/src/web/feedback.php
+++ judge/src/web/feedback.php
@@ -58,8 +58,9 @@ if (isset($data['stderrs']) && is_array($data['stderrs'])) {
         if (isset($stderr['message'])) {
             $command = "cd /home/Capstone_Design_Troy/py/ && python3 matching_hyperlink.py " . escapeshellarg($stderr['message']);
             $link = shell_exec($command);
-            $feedback_error = $link;
-            $output = json_decode($link, JSON_UNESCAPED_UNICODE);
+            $output = $link;
+            //$feedback_error = $link;
+            //$output = json_decode($link, JSON_UNESCAPED_UNICODE);
             //$link_results.append(json_decode($link, true));
         }
     }
