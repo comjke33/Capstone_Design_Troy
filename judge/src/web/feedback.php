f354e650f28f8923a38033bb4a8e1a9a92751dca
comjke33 <comjke33@inu.ac.kr>
2025-05-01 14:21:15 +0900

    json_decode link

--- judge/src/web/feedback.php
+++ judge/src/web/feedback.php
@@ -58,7 +58,7 @@ if (isset($data['stderrs']) && is_array($data['stderrs'])) {
         if (isset($stderr['message'])) {
             $command = "cd /home/Capstone_Design_Troy/py/ && python3 matching_hyperlink.py " . escapeshellarg($stderr['message']);
             $link = shell_exec($command);
-            $output = $link;
+            $output = json_decode($link, JSON_UNESCAPED_UNICODE);
             //$link_results.append(json_decode($link, true));
         }
     }
