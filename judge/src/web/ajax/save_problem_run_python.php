2602d7823077aa3b40ecd14599e0c919018ac7d9
Choi Yeon Ho <cyh990218@naver.com>
2025-05-02 14:31:50 +0900

    flowchart, questions_and_codes생성 python 추가_ver18

--- judge/src/web/ajax/save_problem_run_python.php
+++ judge/src/web/ajax/save_problem_run_python.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once("../../include/db_info.inc.php");
 
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))) {
     http_response_code(403);
