4d2ce032108eabbec1ea4e7e388cc44e9fd8183f
Son Seung Min <sonsmmm@naver.com>
2025-05-02 22:49:14 +0900

    9944 test(8)

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -120,6 +120,8 @@ function updateImageForTextarea(index, ta) {
             const img = document.createElement("img");
             //img.src = "/home/Capstone_Design_Troy/test/flowcharts/1234_1.png";
             img.src = "feedback.jpg";
+            console.log("일단 함수 들어옴")
+
             const taRect = ta.getBoundingClientRect();
             const centerPanel = ta.closest(".center-panel");
             const centerRect = centerPanel.getBoundingClientRect();
