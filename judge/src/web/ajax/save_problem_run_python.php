3af49c130e9257bb95302e30a3a4d4e6ec340308
Choi Yeon Ho <cyh990218@naver.com>
2025-05-03 11:02:19 +0900

    flowchart api problem

--- judge/src/web/ajax/save_problem_run_python.php
+++ judge/src/web/ajax/save_problem_run_python.php
@@ -25,11 +25,13 @@ function run_script($cmd) {
         'return_code' => $return_var
     ];
 }
-$env_vars = parse_ini_file("/home/Capstone_Design_Troy/test/env");
-putenv("OPENAI_API_KEY=" . $env_vars["OPENAI_API_KEY"]);
 $results = [];
+$env_vars = parse_ini_file("/home/Capstone_Design_Troy/test/env");
+$api_key = escapeshellarg($env_vars["OPENAI_API_KEY"]);
+$command = "cd /home/Capstone_Design_Troy/test/ && OPENAI_API_KEY=$api_key python3 AIFlowchart.py " . escapeshellarg($problem_id);
+$results[] = run_script($command);
 $results[] = run_script("cd /home/Capstone_Design_Troy/test/ && python3 make_question_and_code.py " . escapeshellarg($description) . ' ' . escapeshellarg($exemplary_code));
-$results[] = run_script("cd /home/Capstone_Design_Troy/test/ && python3 AIFlowchart.py " . escapeshellarg($problem_id));
+#$results[] = run_script("cd /home/Capstone_Design_Troy/test/ && python3 AIFlowchart.py " . escapeshellarg($problem_id));
 
 header("Content-Type: application/json");
 echo json_encode($results, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
