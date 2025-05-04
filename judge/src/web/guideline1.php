252ec84f34edf6277ed773ae8c2720fff2d90683
Son Seung Min <sonsmmm@naver.com>
2025-05-04 18:40:59 +0900

    가이드라인 분리 rollback

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -8,15 +8,9 @@ $tagged_path = "/home/Capstone_Design_Troy/test/total_test/step1_tagged_code.txt
 $guideline_contents = file_get_contents($file_path);
 $tagged_contents = file_get_contents($tagged_path);
 
-$OJ_BLOCK_TREE = parse_blocks_v2($guideline_contents);
+$OJ_BLOCK_TREE = parse_blocks($guideline_contents);
 $OJ_CORRECT_ANSWERS = extract_tagged_blocks($tagged_contents);
 $OJ_SID = "STEP 1";
-
-$problem_id = isset($_GET['problem_id']) ? intval($_GET['problem_id']) : 0;
-$index = isset($_GET['index']) ? intval($_GET['index']) : -1;
-$default_img = "/flowcharts/default.png";
-$index_start = 2;
-$index_end = 4;
 
 include("template/syzoj/guideline1.php");//렌더링 파일 불러오기
 ?>
