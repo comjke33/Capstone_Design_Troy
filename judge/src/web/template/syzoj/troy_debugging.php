13da437a36bc74cf6702e9031ad155ec26f2976a
comjke33 <comjke33@inu.ac.kr>
2025-05-04 23:13:14 +0900

    파일 위치 변경경

--- judge/src/web/template/syzoj/troy_debugging.php
+++ judge/src/web/template/syzoj/troy_debugging.php
@@ -1,8 +1,10 @@
 <?php
 include("include/db_info.inc.php");
 
+$filename = ".txt";
+
 // 인자를 공백으로 구분해 Python 스크립트에 전달
-$command = "cd /home/Capstone_Design_Troy/py/ && python3 make_flowcharts.py " . escapeshellarg();
+$command = "cd /home/Capstone_Design_Troy/py/ && python3 make_flowcharts.py " . escapeshellarg($filename);
 $result = shell_exec($command);
 if ($result === null) {
     echo "<pre>Python 스크립트 실행 실패!</pre>";
