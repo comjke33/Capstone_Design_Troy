d397be985ce8a8fd0ee76570d24449473b8d1a54
comjke33 <comjke33@inu.ac.kr>
2025-05-04 15:16:30 +0900

    update flowchart

--- judge/src/web/get_flowchart_image.php
+++ judge/src/web/get_flowchart_image.php
@@ -16,13 +16,15 @@ if ($problem_id <= 0 || $index < 0) {
 //DB에서 링크를 가져와서 링크를 구현
 $sql = "SELECT png_address FROM flowchart 
         WHERE problem_id = ? 
-        AND start_num <= 7 
-        AND end_num >= 7
+        AND start_num <= ?
+        AND end_num >= ?
         LIMIT 1";
 
-//$res = pdo_query($sql, $problem_id, $index, $index);
-$res = pdo_query($sql, $problem_id);
-$default_img = "/flowcharts/9944_1.png";
+$res = pdo_query($sql, $problem_id, $index_start, $index_end);
+
+// 디버깅 용 default 이미지 설정
+// $res = pdo_query($sql, $problem_id);
+$default_img = "/flowcharts/default.png";
 
 if (count($res) > 0 && !empty($res['png_address'])) {
     $filename =($res['png_address'] . ".png");
@@ -31,5 +33,7 @@ if (count($res) > 0 && !empty($res['png_address'])) {
     $url = $default_img;
 }
 
-$url = $default_img;
+// 디버깅 용 default 이미지 설정
+// $url = $default_img;
+
 echo json_encode(['success' => true, 'url' => $url]);
