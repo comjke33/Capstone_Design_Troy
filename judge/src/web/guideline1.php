0b3ad6bd28c3aa959814d05135fee7619744c054
Son Seung Min <sonsmmm@naver.com>
2025-05-04 19:43:13 +0900

    가이드라인 분리 error fix

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -2,8 +2,8 @@
 include("include/db_info.inc.php");
 include("guideline_common.php"); // ✨ 공통 파일 사용
 
-$file_path = "/home/Capstone_Design_Troy/test/total_test/step2_guideline.txt";
-$tagged_path = "/home/Capstone_Design_Troy/test/total_test/step2_tagged_code.txt";
+$file_path = "/home/Capstone_Design_Troy/test/total_test/step1_guideline.txt";
+$tagged_path = "/home/Capstone_Design_Troy/test/total_test/step1_tagged_code.txt";
 
 $guideline_contents = file_get_contents($file_path);
 $tagged_contents = file_get_contents($tagged_path);
@@ -11,6 +11,12 @@ $tagged_contents = file_get_contents($tagged_path);
 $OJ_BLOCK_TREE = parse_blocks($guideline_contents);
 $OJ_CORRECT_ANSWERS = extract_tagged_blocks($tagged_contents);
 $OJ_SID = "STEP 1";
+
+$problem_id = isset($_GET['problem_id']) ? intval($_GET['problem_id']) : 0;
+$index = isset($_GET['index']) ? intval($_GET['index']) : -1;
+$default_img = "/flowcharts/default.png";
+$index_start = 2;
+$index_end = 4;
 
 include("template/syzoj/guideline1.php");//렌더링 파일 불러오기
 ?>
