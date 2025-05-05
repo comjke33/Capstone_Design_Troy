df5a7187a47a385eb64875ee3667d3e34d07b10a
taewoo <hst0092@inu.ac.kr>
2025-05-05 18:12:49 +0900

    update

--- judge/src/web/ParsedownWithAnchor.php
+++ judge/src/web/ParsedownWithAnchor.php
@@ -2,29 +2,48 @@
 class ParsedownWithAnchor extends ParsedownExtra
 {
     protected function blockHeader($Line)
-{
-    $block = parent::blockHeader($Line);
+    {
+        $block = parent::blockHeader($Line);
 
-    if (isset($block['element']['name']) && preg_match('/^h[1-6]$/', $block['element']['name'])) {
-        $text = $block['element']['text'] ?? '';
-        $id = $this->slugify(strip_tags($text));
+        // 디버깅: 블록 구조 확인
+        error_log("HEADER BLOCK: " . print_r($block, true));
 
-        if (!isset($block['element']['attributes'])) {
-            $block['element']['attributes'] = [];
+        if (isset($block['element']['name']) && preg_match('/^h[1-6]$/', $block['element']['name'])) {
+            $text = $this->flattenText($block['element']['text']);
+            $id = $this->slugify(strip_tags($text));
+
+            // 디버깅: id 확인
+            error_log(">>> GENERATED ID: $id");
+
+            if (!isset($block['element']['attributes'])) {
+                $block['element']['attributes'] = [];
+            }
+
+            $block['element']['attributes']['id'] = $id;
         }
 
-        $block['element']['attributes']['id'] = $id;
+        return $block;
     }
-
-    return $block;
-}
 
     private function slugify($text)
     {
-        error_log("slugify 대상: " . $text);  // 로그 확인
         $text = trim($text);
-        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text);
-        $text = preg_replace('/\s+/', '-', $text);
+        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text); // 문자/숫자/공백만
+        $text = preg_replace('/\s+/', '-', $text);             // 공백을 하이픈으로
         return strtolower($text);
+    }
+
+    private function flattenText($element)
+    {
+        // $element가 배열이면 재귀적으로 text 뽑기
+        if (is_array($element)) {
+            $result = '';
+            foreach ($element as $sub) {
+                $result .= $this->flattenText($sub);
+            }
+            return $result;
+        } else {
+            return (string)$element;
+        }
     }
 }
