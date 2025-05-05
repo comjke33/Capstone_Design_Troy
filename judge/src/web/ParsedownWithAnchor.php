2875cf82b1db8a5bce2213bf112f4757c2487364
taewoo <hst0092@inu.ac.kr>
2025-05-05 18:14:09 +0900

    update

--- judge/src/web/ParsedownWithAnchor.php
+++ judge/src/web/ParsedownWithAnchor.php
@@ -5,20 +5,19 @@ class ParsedownWithAnchor extends ParsedownExtra
     {
         $block = parent::blockHeader($Line);
 
-        // 디버깅: 블록 구조 확인
-        error_log("HEADER BLOCK: " . print_r($block, true));
-
-        if (isset($block['element']['name']) && preg_match('/^h[1-6]$/', $block['element']['name'])) {
-            $text = $this->flattenText($block['element']['text']);
+        // 'text' 키가 없으면 id를 붙일 수 없음 → 조건 추가
+        if (
+            isset($block['element']) &&
+            is_array($block['element']) &&
+            isset($block['element']['text']) &&
+            is_string($block['element']['text'])
+        ) {
+            $text = $block['element']['text'];
             $id = $this->slugify(strip_tags($text));
-
-            // 디버깅: id 확인
-            error_log(">>> GENERATED ID: $id");
 
             if (!isset($block['element']['attributes'])) {
                 $block['element']['attributes'] = [];
             }
-
             $block['element']['attributes']['id'] = $id;
         }
 
@@ -28,22 +27,10 @@ class ParsedownWithAnchor extends ParsedownExtra
     private function slugify($text)
     {
         $text = trim($text);
-        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text); // 문자/숫자/공백만
-        $text = preg_replace('/\s+/', '-', $text);             // 공백을 하이픈으로
+        // 한글/영문/숫자/공백만 허용
+        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text);
+        // 공백을 하이픈으로 치환
+        $text = preg_replace('/\s+/', '-', $text);
         return strtolower($text);
-    }
-
-    private function flattenText($element)
-    {
-        // $element가 배열이면 재귀적으로 text 뽑기
-        if (is_array($element)) {
-            $result = '';
-            foreach ($element as $sub) {
-                $result .= $this->flattenText($sub);
-            }
-            return $result;
-        } else {
-            return (string)$element;
-        }
     }
 }
