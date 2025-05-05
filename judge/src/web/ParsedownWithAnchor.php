e7167ae0b678899f31d90074f6ccf1d2cd1d09b8
taewoo <hst0092@inu.ac.kr>
2025-05-05 18:15:26 +0900

    update

--- judge/src/web/ParsedownWithAnchor.php
+++ judge/src/web/ParsedownWithAnchor.php
@@ -5,20 +5,31 @@ class ParsedownWithAnchor extends ParsedownExtra
     {
         $block = parent::blockHeader($Line);
 
-        // 'text' 키가 없으면 id를 붙일 수 없음 → 조건 추가
         if (
             isset($block['element']) &&
-            is_array($block['element']) &&
-            isset($block['element']['text']) &&
-            is_string($block['element']['text'])
+            is_array($block['element'])
         ) {
-            $text = $block['element']['text'];
-            $id = $this->slugify(strip_tags($text));
+            // h2 텍스트 가져오기
+            $text = '';
 
-            if (!isset($block['element']['attributes'])) {
-                $block['element']['attributes'] = [];
+            if (isset($block['element']['text']) && is_string($block['element']['text'])) {
+                $text = $block['element']['text'];
+            } elseif (
+                isset($block['element']['handler']['argument']) &&
+                is_string($block['element']['handler']['argument'])
+            ) {
+                $text = $block['element']['handler']['argument'];
             }
-            $block['element']['attributes']['id'] = $id;
+
+            if ($text !== '') {
+                $id = $this->slugify(strip_tags($text));
+
+                if (!isset($block['element']['attributes'])) {
+                    $block['element']['attributes'] = [];
+                }
+
+                $block['element']['attributes']['id'] = $id;
+            }
         }
 
         return $block;
@@ -27,10 +38,8 @@ class ParsedownWithAnchor extends ParsedownExtra
     private function slugify($text)
     {
         $text = trim($text);
-        // 한글/영문/숫자/공백만 허용
-        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text);
-        // 공백을 하이픈으로 치환
-        $text = preg_replace('/\s+/', '-', $text);
+        $text = preg_replace('/[^\p{L}\p{N}\s]/u', '', $text); // 한글, 숫자, 문자만
+        $text = preg_replace('/\s+/', '-', $text);             // 공백 → 하이픈
         return strtolower($text);
     }
 }
