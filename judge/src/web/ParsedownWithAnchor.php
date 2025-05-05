2aa3fe41d5bed1f00d7e4168882c7556d0244d0c
taewoo <hst0092@inu.ac.kr>
2025-05-05 17:43:30 +0900

    update

--- judge/src/web/ParsedownWithAnchor.php
+++ judge/src/web/ParsedownWithAnchor.php
@@ -2,22 +2,32 @@
 class ParsedownWithAnchor extends ParsedownExtra {
     function blockHeader($Line) {
         $block = parent::blockHeader($Line);
-        if (isset($block['element']['text'])) {
+
+        if (isset($block['element']['name']) && isset($block['element']['text'])) {
             $text = $block['element']['text'];
+
+            // 'text'가 어레이인 경우 내부 'text'만 추출
+            if (is_array($text) && isset($text['text'])) {
+                $text = $text['text'];
+            }
+
             $id = $this->slugify(strip_tags($text));
-            // 기존 속성이 있으면 병합
+
             if (!isset($block['element']['attributes'])) {
                 $block['element']['attributes'] = [];
             }
+
             $block['element']['attributes']['id'] = $id;
         }
+
         return $block;
     }
 
     private function slugify($text) {
-        $text = strtolower(trim($text));
-        $text = preg_replace('/[^\p{L}\p{N}\s-]+/u', '', $text); // 한글, 영문, 숫자, 공백, 하이픈만
-        $text = preg_replace('/[\s]+/', '-', $text);             // 공백을 하이픈으로
+        $text = strip_tags($text);
+        $text = mb_strtolower(trim($text));
+        $text = preg_replace('/[^\p{L}\p{N}\s-]+/u', '', $text);  // 한글 포함
+        $text = preg_replace('/[\s]+/u', '-', $text);             // 공백 → 하이픈
         return $text;
     }
 }
