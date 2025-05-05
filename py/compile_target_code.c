e6a16fc65965b6fbda5e65779d869e7d213ad39b
zxccyh <cyh990218@naver.com>
2025-05-05 05:20:19 +0000

    이미지 확인

--- py/compile_target_code.c
+++ py/compile_target_code.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+void set_value(char *str) {
+    *str = 'A';
+}
+
 int main() {
-    printf("Hello World")
+    int arr[5];
+    arr[5] = 100;  // 배열 인덱스 초과
+    int num;
+    scanf("%lf", &num);  // 형식 지정자 오류 (int에 %lf)
+    int id = 42;
+    set_value(&id);  // 포인터 타입 불일치 (int* → char*)
+    return 0;
 }
