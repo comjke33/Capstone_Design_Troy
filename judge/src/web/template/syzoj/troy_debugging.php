39d6dce92800853b7d1dcd3aa62cae2601413937
comjke33 <comjke33@inu.ac.kr>
2025-05-06 23:54:32 +0900

    경로 디버깅깅

--- judge/src/web/template/syzoj/troy_debugging.php
+++ judge/src/web/template/syzoj/troy_debugging.php
@@ -5,28 +5,14 @@ $problem_id = isset($_GET['problem_id']) ? intval($_GET['problem_id']) : 0;
 $problem_id = "1000";
 
 // 인자를 공백으로 구분해 Python 스크립트에 전달
-$tagged_guideline = "./tagged_guideline/" . $problem_id . ".txt";
-$tagged_guideline = "./tagged_guideline/";
+$tagged_guideline = "../src/web/tagged_guideline/" . $problem_id . ".txt";
+// $tagged_guideline = "../src/web/tagged_guideline/";
 // problem을 가져오는 SQL 쿼리
 $sql = "SELECT description FROM problem WHERE problem_id = ?";
 $problem = pdo_query($sql, $problem_id);
 $desc = $problem[0][0];
 
-$output_dir = "./flowcharts/";
-
-echo "<pre>$problem_id $tagged_guideline $desc $output_dir </pre>";
-// $problem = "problem.txt";
-$files = scandir($tagged_guideline);
-
-echo "<h3>폴더 내 파일 목록:</h3>";
-echo "<ul>";
-foreach ($files as $file) {
-    // . 과 .. 은 현재 폴더와 상위 폴더니까 제외
-    if ($file != "." && $file != "..") {
-        echo "<li>$file</li>";
-    }
-}
-echo "</ul>";
+$output_dir = "../src/web/flowcharts/";
 
 echo "<pre>Python 스크립트 실행 중...</pre>";
 
