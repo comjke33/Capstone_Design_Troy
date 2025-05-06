d76e877618e881273c487007796728cfe8b6eced
Son Seung Min <sonsmmm@naver.com>
2025-05-06 19:13:55 +0900

    guideline update

--- judge/src/web/guideline2.php
+++ judge/src/web/guideline2.php
@@ -2,8 +2,8 @@
 include("include/db_info.inc.php");
 include("guideline_common.php"); // ✨ 공통 파일 사용
 
-$file_path = "/home/Capstone_Design_Troy/test/total_test/step1_guideline.txt";
-$tagged_path = "/home/Capstone_Design_Troy/test/total_test/step1_tagged_code.txt";
+$file_path = "/home/Capstone_Design_Troy/test/total_test/step2_guideline.txt";
+$tagged_path = "/home/Capstone_Design_Troy/test/total_test/step2_tagged_code.txt";
 
 $guideline_contents = file_get_contents($file_path);
 $tagged_contents = file_get_contents($tagged_path);
@@ -18,5 +18,5 @@ $default_img = "/flowcharts/default.png";
 $index_start = 2;
 $index_end = 4;
 
-include("template/syzoj/guideline1.php");//렌더링 파일 불러오기
+include("template/syzoj/guideline2.php");//렌더링 파일 불러오기
 ?>
