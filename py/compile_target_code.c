fc366ed8f4f8a54a03b56a953c756467c3eff441
zxccyh <cyh990218@naver.com>
2025-05-07 22:52:16 +0900

    d

--- py/compile_target_code.c
+++ py/compile_target_code.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-int a(int _a, int _b) {
-    return _a + _b;
-}
-
 int main() {
-    int c = a(10,20);
-    printf("%d", c);
-    
-    scanf("%d", c);
-        
-    int limit[10] = {10,};
-    
-    printf("%d", limit[11]);
+    int num;
+    scanf("%x", &num);
+    printf("%d\n", num);
+    return 0;
 }
