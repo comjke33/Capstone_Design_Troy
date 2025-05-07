f5b1311153a9440e0b9023bc8327ad9346dad4f6
Choi Yeon Ho <cyh990218@naver.com>
2025-05-07 20:15:27 +0900

    문제 등록(2)

--- judge/src/web/add_problem/guest-client4.py
+++ judge/src/web/add_problem/guest-client4.py
@@ -1,4 +1,8 @@
 import socket, json
+import sys
+
+if len(sys.argv) > 1:
+    pid = sys.argv[1]
 
 HOST = '192.168.0.60'
 PORT = 9966
@@ -51,8 +55,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     step_names = ['step1', 'step2', 'step3']
     for i, step in enumerate(step_names, 1):
         # step1_tagged_code.txt, step1_guideline.txt와 같은 파일로 저장
-        tagged_code_filename = f"./total_test/{step}_tagged_code.txt"
-        guideline_filename = f"./total_test/{step}_guideline.txt"
+        tagged_code_filename = f"./../tagged_code/{pid}_{step}_tagged_code.txt"
+        guideline_filename = f"./../tagged_guideline/{pid}_{step}_guideline.txt"
         
         with open(tagged_code_filename, "w", encoding="utf-8") as f:
             f.write(guide_data.get(f"{step}_tagged_code", ""))
