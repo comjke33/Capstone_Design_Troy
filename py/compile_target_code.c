2a49c67a99c62cb53f714bd5dac47c12333fdc24
zxccyh <cyh990218@naver.com>
2025-05-10 22:09:01 +0900

    test

--- py/compile_target_code.c
+++ py/compile_target_code.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+int get_total(int score1, int score2) {
+    int total = score1 + score2
+    return total;
+}
+
 int main() {
-    int num;
-    scanf("%x", &num);
-    printf("%d\n", num);
+    scanf("%d %d", &a, &b);  // a, b 선언 누락
+    int total = get_total(a, b);
+    printf("Total: %d\n", total);
     return 0;
 }
