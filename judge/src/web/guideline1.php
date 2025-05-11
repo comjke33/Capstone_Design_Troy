3d73afb0888af33231fdeb5ee922f9386610e1d1
Son Seung Min <sonsmmm@naver.com>
2025-05-11 11:52:14 +0900

    default.png delete

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -14,7 +14,6 @@ $OJ_SID = "STEP 1";
 
 $problem_id = isset($_GET['problem_id']) ? intval($_GET['problem_id']) : 0;
 $index = isset($_GET['index']) ? intval($_GET['index']) : -1;
-// $default_img = "./flowcharts/default.png";
 $index_start = 0;
 $index_end = 0;
 
