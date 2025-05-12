b79c106f5c262c28b2c87c55cece7a9a577fb616
Son Seung Min <sonsmmm@naver.com>
2025-05-12 23:41:14 +0900

    fixed feddback UI

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -35,7 +35,7 @@ include("../../guideline_common.php");
 <div class="main-layout">
     <!-- 좌측 패널 -->
     <div class="left-panel">
-        <img id="flowchart_image" src="../../image/basic.png">
+        <img id="flowchart_image">
     </div>
 
 
