ea7f2917a39a3fa6a3506a6d73ad1032f76eb45b
Son Seung Min <sonsmmm@naver.com>
2025-05-13 23:43:54 +0900

    status.php apply

--- judge/src/web/get_user_id.php
+++ judge/src/web/get_user_id.php
@@ -2,31 +2,18 @@
 require_once "include/db_info.inc.php";
 header("Content-Type: application/json");
 
-// 선택할 user_id 목록
-$allowed_user_ids = [
-    'admin',
-    'endtoend',
-    'end_to_end',
-    'ERRORMAN',
-    'ERRORMON',
-    'errortest',
-    'error_test',
-    'error_test1',
-    'guideline_user1',
-    'guideline_user2',
-    // 'notguideline_user1',
-    // 'notguideline_user2',
-    'notification_test',
-    'notification_test2',
-    'sonson',
-    'test',
-    'test1',
-    'test2',
-    'test3',
-    'test4',
-    'test5',
-    'zxccyh',
-    'zxczxc'
-];
+// Initialize the allowed_user_ids array
+$allowed_user_ids = [];
 
+// Fetch user_ids from the database
+$query = "SELECT user_id FROM users";
+$result = mysqli_query($db, $query);
+
+// Loop through the results and populate the $allowed_user_ids array
+while ($row = mysqli_fetch_assoc($result)) {
+    $allowed_user_ids[] = $row['user_id'];
+}
+
+// Return the $allowed_user_ids array (if needed for testing/debugging)
 return $allowed_user_ids;
+?>
