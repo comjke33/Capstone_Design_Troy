a836a384c2d73a305ecd736fd0d33744e30f60f4
Son Seung Min <sonsmmm@naver.com>
2025-05-13 23:27:34 +0900

    status.php GET user_id

--- judge/src/web/status.php
+++ judge/src/web/status.php
@@ -10,6 +10,8 @@ require_once('./include/db_info.inc.php');
 require_once('./include/memcache.php');
 require_once('./include/setlang.php');
 require_once("./include/my_func.inc.php");
+//허락된 유저 id 가져오기
+require_once ("./get_user_id.php");
 $view_title = "$MSG_STATUS";
 
 if (isset($OJ_LANG)) {
@@ -509,21 +511,27 @@ for ($i=0; $i<$rows_cnt; $i++) {
   else
     $view_status[$i][8]= $row['in_date'];
 
-    if (!isset($cid)) { // 대회 문제가 아닌 경우에만 버튼 출력
-      $sid = urlencode($row['solution_id']);
-      $pid = urlencode($row['problem_id']);
-      if ($row['result'] != 4) {  // Accepted가 아닌 경우
-          $view_status[$i][10] = "<a target=\"_self\" href=\"feedback.php?solution_id={$sid}&problem_id={$pid}\" class=\"ui orange mini button\">피드백 보기</a>";
-      } else { // Accepted인 경우
-        $view_status[$i][10] = "
-        <button class='toggle-similar ui blue mini button' data-sid='{$sid}'>유사문제 추천</button>
-        <div id='similar-{$sid}' class='similar-box' style='display:none; margin-top:5px;'></div>
-      ";
+    $current_user_id = $_SESSION['user_id'];
+
+   // Only display buttons for allowed users
+  if (in_array($current_user_id, $allowed_user_ids)) {
+      if (!isset($cid)) { // 대회 문제가 아닌 경우에만 버튼 출력
+          $sid = urlencode($row['solution_id']);
+          $pid = urlencode($row['problem_id']);
+          if ($row['result'] != 4) {  // Accepted가 아닌 경우
+              $view_status[$i][10] = "<a target=\"_self\" href=\"feedback.php?solution_id={$sid}&problem_id={$pid}\" class=\"ui orange mini button\">피드백 보기</a>";
+          } else { // Accepted인 경우
+              $view_status[$i][10] = "
+              <button class='toggle-similar ui blue mini button' data-sid='{$sid}'>유사문제 추천</button>
+              <div id='similar-{$sid}' class='similar-box' style='display:none; margin-top:5px;'></div>
+              ";
+          }
+      } else {
+          $view_status[$i][10] = "-"; // 대회 문제인 경우 출력 안 함
       }
   } else {
-      $view_status[$i][10] = "-"; // 대회 문제인 경우 출력 안 함
+      $view_status[$i][10] = "-"; // 버튼을 출력하지 않음
   }
-  
   
 
 }
