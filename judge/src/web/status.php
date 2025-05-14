369d8a530f77d43480ef728f104f7c220c78a9df
Son Seung Min <sonsmmm@naver.com>
2025-05-14 14:31:33 +0900

    status.php allowed_user6

--- judge/src/web/status.php
+++ judge/src/web/status.php
@@ -511,19 +511,23 @@ for ($i=0; $i<$rows_cnt; $i++) {
     $view_status[$i][8]= $row['in_date'];
 
     // 예시로, 특정 user_id만 버튼을 보이게 하려면
-    $allowed_user_id = 'guideline_user1';
+    $disallowed_user_id = 
+      [ 'noguideline_user1',
+        'noguideline_user2'
+      ];
 
-    if (!isset($cid) && $_SESSION[$OJ_NAME.'_'.'user_id'] == $allowed_user_id) { // 대회 문제가 아닌 경우에만 버튼 출력
-      $sid = urlencode($row['solution_id']);
-      $pid = urlencode($row['problem_id']);
+    if (!isset($cid) && !in_array($_SESSION[$OJ_NAME.'_'.'user_id'], $disallowed_user_id)) { 
+    // 대회 문제가 아닌 경우 또는 disallowed_user_id에 포함되지 않은 경우에만 버튼 출력
+    $sid = urlencode($row['solution_id']);
+    $pid = urlencode($row['problem_id']);
       if ($row['result'] != 4) {  // Accepted가 아닌 경우
           $view_status[$i][10] = "<a target=\"_self\" href=\"feedback.php?solution_id={$sid}&problem_id={$pid}\" class=\"ui orange mini button\">피드백 보기</a>";
       } else { // Accepted인 경우
-        $view_status[$i][10] = "
-        <button class='toggle-similar ui blue mini button' data-sid='{$sid}'>유사문제 추천</button>
-        <div id='similar-{$sid}' class='similar-box' style='display:none; margin-top:5px;'></div>
-      ";
-        }
+          $view_status[$i][10] = "
+          <button class='toggle-similar ui blue mini button' data-sid='{$sid}'>유사문제 추천</button>
+          <div id='similar-{$sid}' class='similar-box' style='display:none; margin-top:5px;'></div>
+          ";
+      }
     } else {
         $view_status[$i][10] = "-"; // 대회 문제인 경우 출력 안 함
     }
