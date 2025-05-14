4faa91d5a16b9a95bf89fa84fa464d045b2eb435
Son Seung Min <sonsmmm@naver.com>
2025-05-14 15:47:04 +0900

    guideline3.php 답안확인 -> 예제확인

--- judge/src/web/template/syzoj/guideline3.php
+++ judge/src/web/template/syzoj/guideline3.php
@@ -65,11 +65,8 @@ include("../../guideline_common.php");
                     $html .= "<div class='code-line'>{$line}</div>";
                     $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' {$disabled}></textarea>";
 
-                    // 라인 번호에 맞는 이미지를 업데이트하기 위한 스크립트 추가
-                    // $html .= "<script>updateImageForTextarea({$answer_index + 1}, document.getElementById('ta_{$answer_index}'));</script>";  // 라인 번호에 맞춰 이미지 업데이트
-
                     $html .= "<button onclick='submitAnswer({$answer_index})' id='submit_btn_{$answer_index}' class='submit-button'>제출</button>";
-                    $html .= "<button onclick='showAnswer({$answer_index})' id='answer_btn_{$answer_index}' class='answer-button'>답안 확인</button>";
+                    $html .= "<button onclick='showAnswer({$answer_index})' id='answer_btn_{$answer_index}' class='answer-button'>예제 확인</button>";
                     $html .= "<button onclick='showFeedback({$answer_index})' id='feedback_btn_{$answer_index}' class='feedback-button'>피드백 보기</button>";
 
                     $html .= "<div id='answer_area_{$answer_index}' class='answer-area' style='display:none; margin-top: 10px;'></div>";
