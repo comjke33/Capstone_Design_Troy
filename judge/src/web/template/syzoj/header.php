7b94281ca434fc6739c608cd37f9b2ce06984b3a
Son Seung Min <sonsmmm@naver.com>
2025-05-14 16:07:07 +0900

    test again header.php

--- judge/src/web/template/syzoj/header.php
+++ judge/src/web/template/syzoj/header.php
@@ -1,6 +1,6 @@
 <?php
-//허가된 유저들만 사용가능하게
-include('../../allowed_users.php');
+// allowed_users.php 파일을 포함하여 배열을 불러옴
+include('allowed_users.php');
 ?>
 
 <?php
@@ -162,6 +162,10 @@ html, body {
 </script>
 </head>
 
+<?php
+// allowed_users.php 파일을 포함하여 배열을 불러옴
+include('allowed_users.php');
+?>
 
 <?php
         if(!isset($_GET['spa'])){
@@ -236,33 +240,41 @@ html, body {
                     <?php }  ?>
             <?php }  ?>
             <?php echo $sql_news_menu_result_html; ?>
+
+            <?php
+            // allowed_users.php 파일을 포함하여 배열을 불러옴
+            include('allowed_users.php');
+            ?>
+
             <div class="right menu">
+                
+
                 <?php
-                    $allowed_user_id = [
-                    'admin', 
-                    'endtoend', 
-                    'end_to_end', 
-                    'ERRORMAN', 
-                    'ERRORMON', 
-                    'errortest', 
-                    'error_test', 
-                    'error_test1', 
-                    'guideline_user1', 
-                    'guideline_user2', 
-                    // 'notguideline_user1', 
-                    // 'notguideline_user2', 
-                    'notification_test', 
-                    'notification_test2', 
-                    'sonson', 
-                    'test', 
-                    'test1', 
-                    'test2', 
-                    'test3', 
-                    'test4', 
-                    'test5', 
-                    'zxccyh', 
-                    'zxczxc'
-                    ];
+                    // $allowed_user_id = [
+                    // 'admin', 
+                    // 'endtoend', 
+                    // 'end_to_end', 
+                    // 'ERRORMAN', 
+                    // 'ERRORMON', 
+                    // 'errortest', 
+                    // 'error_test', 
+                    // 'error_test1', 
+                    // 'guideline_user1', 
+                    // 'guideline_user2', 
+                    // // 'notguideline_user1', 
+                    // // 'notguideline_user2', 
+                    // 'notification_test', 
+                    // 'notification_test2', 
+                    // 'sonson', 
+                    // 'test', 
+                    // 'test1', 
+                    // 'test2', 
+                    // 'test3', 
+                    // 'test4', 
+                    // 'test5', 
+                    // 'zxccyh', 
+                    // 'zxczxc'
+                    // ];
 
                 // allowed_user_id에 포함된 사용자만 알림 버튼을 보이도록 조건 추가
                 if (in_array($_SESSION[$OJ_NAME.'_'.'user_id'], $allowed_user_id)) {
