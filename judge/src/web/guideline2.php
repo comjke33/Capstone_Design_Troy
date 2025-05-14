4b3821db80f1990e88ca313951e6f0ecf5cc1f5a
Son Seung Min <sonsmmm@naver.com>
2025-05-15 02:56:29 +0900

    guideline2, 3.php update

--- judge/src/web/guideline2.php
+++ judge/src/web/guideline2.php
@@ -2,8 +2,11 @@
 include("include/db_info.inc.php");
 include("guideline_common.php"); // ✨ 공통 파일 사용
 
-$file_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_guideline/1256_step2.txt";
-$tagged_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_code/1256_step2.txt";
+// $problem_id = 1295; 
+$problem_id = isset($_GET['problem_id']) ? intval($_GET['problem_id']) : 0;
+
+$file_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_guideline/{$problem_id}_step2.txt";
+$tagged_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_code/{$problem_id}_step2.txt";
 
 $guideline_contents = file_get_contents($file_path);
 $tagged_contents = file_get_contents($tagged_path);
