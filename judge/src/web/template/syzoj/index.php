90b3758a318b3d9bb0ed54211bd404be8318f4a9
Son Seung Min <sonsmmm@naver.com>
2025-05-16 02:08:59 +0900

    index.php ui fix

--- judge/src/web/template/syzoj/index.php
+++ judge/src/web/template/syzoj/index.php
@@ -23,73 +23,20 @@
         </div>
       <?php } ?>
 
-      <h4 class="ui top attached block header"><i class="ui info icon"></i><?php echo $MSG_NEWS; ?></h4>
+      <!-- 검색 -->
+      <h4 class="ui top attached block header"><i class="ui search icon"></i><?php echo $MSG_SEARCH; ?></h4>
       <div class="ui bottom attached segment">
-        <table class="ui very basic table">
-          <thead>
-            <tr><th><?php echo $MSG_TITLE; ?></th><th><?php echo $MSG_TIME; ?></th></tr>
-          </thead>
-          <tbody>
-            <?php
-              $sql_news_main = "SELECT * FROM news WHERE defunct!='Y' AND title!='faqs.cn' ORDER BY importance DESC, time DESC LIMIT 10";
-              $result_news = mysql_query_cache($sql_news_main);
-              if ($result_news) {
-                foreach ($result_news as $row) {
-                  echo "<tr><td><a href='viewnews.php?id={$row['news_id']}'>{$row['title']}</a></td><td>{$row['time']}</td></tr>";
-                }
-              } else {
-                echo "<tr><td colspan='2'>check database connection or account!</td></tr>";
-              }
-            ?>
-          </tbody>
-        </table>
-      </div>
-
-      <?php
-        $month_id = mysql_query_cache("SELECT solution_id FROM solution WHERE in_date<DATE_ADD(CURDATE(), INTERVAL -DAY(CURDATE())+1 DAY) ORDER BY solution_id DESC LIMIT 1;");
-        $month_id = (!empty($month_id) && isset($month_id[0][0])) ? $month_id[0][0] : 0;
-
-        if(isset($NOIP_flag[0]) && $NOIP_flag[0]==0) {
-          $view_month_rank = mysql_query_cache("SELECT user_id,nick,COUNT(DISTINCT problem_id) ac FROM solution WHERE solution_id>$month_id AND problem_id>0 AND user_id NOT IN ($OJ_RANK_HIDDEN) AND result=4 GROUP BY user_id,nick ORDER BY ac DESC LIMIT 10");
-          if(!empty($view_month_rank)) {
-      ?>
-            <h4 class="ui top attached block header"><i class="ui star icon"></i>이달의 우수생</h4>
-            <div class="ui bottom attached segment">
-              <table class="ui very basic center aligned table">
-                <tbody>
-                <?php foreach ($view_month_rank as $row) {
-                    echo "<tr><td><a href='userinfo.php?user=".htmlentities($row[0], ENT_QUOTES, "UTF-8")."'>".htmlentities($row[0], ENT_QUOTES, "UTF-8")."</a></td><td>{$row[1]}</td><td>{$row[2]}</td></tr>";
-                } ?>
-                </tbody>
-              </table>
+        <form action="problem.php" method="get">
+          <div class="ui search">
+            <div class="ui left icon input">
+              <input class="prompt" type="text" placeholder="<?php echo $MSG_PROBLEM_ID; ?> …" name="id">
+              <i class="search icon"></i>
             </div>
-      <?php }} ?>
-
-      <h4 class="ui top attached block header"><i class="ui star icon"></i><?php echo $OJ_INDEX_NEWS_TITLE; ?></h4>
-      <div class="ui bottom attached segment">
-        <table class="ui very basic left aligned table">
-          <tbody>
-            <?php
-              $sql_news_index = "SELECT * FROM news WHERE defunct!='Y' AND title='$OJ_INDEX_NEWS_TITLE' ORDER BY importance ASC, time DESC LIMIT 10";
-              $result_news = mysql_query_cache($sql_news_index);
-              if ($result_news) {
-                foreach ($result_news as $row) {
-                  echo "<tr><td>".bbcode_to_html($row['content'])."</td></tr>";
-                }
-              }
-            ?>
-            <tr><td>
-              <center>Recent submission: <?php echo $speed; ?>
-              <div id="submission" style="width:80%;height:300px"></div></center>
-            </td></tr>
-          </tbody>
-        </table>
+          </div>
+        </form>
       </div>
-    </div>
 
-    <!-- 오른쪽 사이드 섹션 -->
-    <div class="right floated five wide column">
-
+      <!-- 최근 문제 -->
       <h4 class="ui top attached block header"><i class="ui rss icon"></i> <?php echo $MSG_RECENT_PROBLEM; ?> </h4>
       <div class="ui bottom attached segment">
         <table class="ui very basic center aligned table">
@@ -112,36 +59,30 @@
         </table>
       </div>
 
-      <h4 class="ui top attached block header"><i class="ui search icon"></i><?php echo $MSG_SEARCH; ?></h4>
-      <div class="ui bottom attached segment">
-        <form action="problem.php" method="get">
-          <div class="ui search">
-            <div class="ui left icon input">
-              <input class="prompt" type="text" placeholder="<?php echo $MSG_PROBLEM_ID; ?> …" name="id">
-              <i class="search icon"></i>
-            </div>
-          </div>
-        </form>
-      </div>
+      <!-- 이달의 우수생 -->
+      <?php
+        $month_id = mysql_query_cache("SELECT solution_id FROM solution WHERE in_date<DATE_ADD(CURDATE(), INTERVAL -DAY(CURDATE())+1 DAY) ORDER BY solution_id DESC LIMIT 1;");
+        $month_id = (!empty($month_id) && isset($month_id[0][0])) ? $month_id[0][0] : 0;
 
-      <h4 class="ui top attached block header"><i class="ui calendar icon"></i><?php echo $MSG_RECENT_CONTEST; ?></h4>
-      <div class="ui bottom attached center aligned segment">
-        <table class="ui very basic center aligned table">
-          <thead><tr><th><?php echo $MSG_CONTEST_NAME; ?></th><th><?php echo $MSG_START_TIME; ?></th></tr></thead>
-          <tbody>
-            <?php
-              $sql_contests = "SELECT * FROM contest WHERE defunct='N' ORDER BY contest_id DESC LIMIT 5";
-              $result_contests = mysql_query_cache($sql_contests);
-              foreach ($result_contests as $row) {
-                echo "<tr><td><a href='contest.php?cid={$row['contest_id']}'>{$row['title']}</a></td><td>".substr($row['start_time'],5,5)."</td></tr>";
-              }
-            ?>
-          </tbody>
-        </table>
-      </div>
+        if(isset($NOIP_flag[0]) && $NOIP_flag[0]==0) {
+          $view_month_rank = mysql_query_cache("SELECT user_id,nick,COUNT(DISTINCT problem_id) ac FROM solution WHERE solution_id>$month_id AND problem_id>0 AND user_id NOT IN ($OJ_RANK_HIDDEN) AND result=4 GROUP BY user_id,nick ORDER BY ac DESC LIMIT 10");
+          if(!empty($view_month_rank)) {
+      ?>
+            <h4 class="ui top attached block header"><i class="ui star icon"></i>이달의 우수생</h4>
+            <div class="ui bottom attached segment">
+              <table class="ui very basic center aligned table">
+                <tbody>
+                <?php foreach ($view_month_rank as $row) {
+                    echo "<tr><td><a href='userinfo.php?user=".htmlentities($row[0], ENT_QUOTES, "UTF-8")."'>".htmlentities($row[0], ENT_QUOTES, "UTF-8")."</a></td><td>{$row[1]}</td><td>{$row[2]}</td></tr>";
+                } ?>
+                </tbody>
+              </table>
+            </div>
+      <?php }} ?>
     </div>
   </div>
 </div>
+
 
 <?php include("template/$OJ_TEMPLATE/footer.php"); ?>
 
