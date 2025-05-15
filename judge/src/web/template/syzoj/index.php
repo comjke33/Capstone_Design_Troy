2d2224ef7de21ce5ba4f25f2a7fd9507c1b795cf
Son Seung Min <sonsmmm@naver.com>
2025-05-16 02:19:06 +0900

    slide.css fix

--- judge/src/web/template/syzoj/index.php
+++ judge/src/web/template/syzoj/index.php
@@ -97,15 +97,19 @@ function showSlide(index) {
   slides.forEach((slide, i) => slide.classList.toggle('active', i === index));
   dots.forEach((dot, i) => dot.classList.toggle('active', i === index));
 }
+
 function nextSlide() {
   currentIndex = (currentIndex + 1) % slides.length;
   showSlide(currentIndex);
 }
+
 function prevSlide() {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
   showSlide(currentIndex);
 }
+
 autoPlayInterval = setInterval(nextSlide, 5000);
+
 dots.forEach(dot => {
   dot.addEventListener('click', () => {
     currentIndex = parseInt(dot.dataset.index);
@@ -114,6 +118,7 @@ dots.forEach(dot => {
     autoPlayInterval = setInterval(nextSlide, 5000);
   });
 });
+
 document.querySelector('.carousel').addEventListener('mouseenter', () => clearInterval(autoPlayInterval));
 document.querySelector('.carousel').addEventListener('mouseleave', () => autoPlayInterval = setInterval(nextSlide, 5000));
 </script>
