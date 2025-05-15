17467192b4e58e6273d4f9c8e350d1dd4da3449e
Son Seung Min <sonsmmm@naver.com>
2025-05-15 23:23:52 +0900

    unfade ui three column grid

--- judge/src/web/template/sidebar/index.php
+++ judge/src/web/template/sidebar/index.php
@@ -3,63 +3,22 @@
 <div class="padding">
     <div class="ui three column grid">
         <div class="eleven wide column">
-            <!-- 홈 화면 공지사항 부분 -->
-            <!-- <h4 class="ui top attached block header"><i class="ui info icon"></i><?php echo $MSG_NEWS;?></h4> -->
-            <!-- <div class="ui bottom attached segment">
-                <table class="ui very basic table">
 
-                    <!-- 최근 문제 부분-->
-                    <!-- <thead>
-                        <tr>
-                            <th><?php echo $MSG_TITLE;?></th>
-                            <th><?php echo $MSG_TIME;?></th>
-                        </tr>
-                    </thead> -->
-                    <!-- <tbody>
-                        <?php
-                        $sql_news = "select * FROM `news` WHERE `defunct`!='Y' AND `title`!='faqs.cn' ORDER BY `importance` ASC,`time` DESC LIMIT 10";
-                        $result_news = mysql_query_cache( $sql_news );
-                        if ( $result_news ) {
-                            foreach ( $result_news as $row ) {
-                                echo "<tr>"."<td>"
-                                    ."<a href=\"viewnews.php?id=".$row["news_id"]."\">"
-                                    .$row["title"]."</a></td>"
-                                    ."<td>".$row["time"]."</td>"."</tr>";
-                            }
-                        }else{
-                            echo "check database connection or account ! ";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>  -->
+        </div>
+            <h4 class="ui top attached block header"><i class="ui search icon"></i><?php echo $MSG_SEARCH;?></h4>
+            <div class="ui bottom attached segment">
+                <form action="problem.php" method="get">
+                    <div class="ui search" style="width: 100%; ">
+                        <div class="ui left icon input" style="width: 100%; ">
+                            <input class="prompt" style="width: 100%; " type="text" placeholder="<?php echo $MSG_PROBLEM_ID ;?> …" name="id">
+                            <i class="search icon"></i>
+                        </div>
+                        <div class="results" style="width: 100%; "></div>
+                    </div>
+                </form>
+            </div>            
+
 <?php
-
-// 문제를 푼 제출 및 통과 그래프
-// $month_id=mysql_query_cache("select solution_id from solution where  in_date<date_add(curdate(),interval -day(curdate())+1 DAY) order by solution_id desc limit 1;");
-// if(!empty( $month_id) && isset($month_id[0][0]) ) $month_id=$month_id[0][0];else $month_id=0;
-// if($NOIP_flag[0]==0)$view_month_rank=mysql_query_cache("select user_id,nick,count(distinct(problem_id)) ac from solution where solution_id>$month_id and problem_id>0  and user_id not in (".$OJ_RANK_HIDDEN.")  and result=4 group by user_id,nick order by ac desc limit 10");
-//             if ( !empty($view_month_rank) ) {
-//         ?>
-//             <h4 class="ui top attached block header"><i class="ui star icon"></i><?php echo "本月之星"?></h4>
-//             <div class="ui bottom attached segment">
-//                 <table class="ui very basic center aligned table" style="table-layout: fixed; ">
-//                     <tbody>
-//         <?php
-//                             foreach ( $view_month_rank as $row ) {
-//                                     echo "<tr>".
-//                                             "<td><a target='_blank' href='userinfo.php?user=".htmlentities($row[0],ENT_QUOTES,"UTF-8")."'>".htmlentities($row[0],ENT_QUOTES,"UTF-8")."</a></td>".
-//                                             "<td>".($row[1])."</td>".
-//                                             "<td>".($row[2])."</td>".
-//                                             "</tr>";
-//                             }
-//         ?>
-//                     </tbody>
-//                 </table>
-//             </div>
-//         <?php
-//             }
-// ?>
 
             <h4 class="ui top attached block header"><i class="ui star icon"></i><?php echo $OJ_INDEX_NEWS_TITLE;?></h4>
             <div class="ui bottom attached segment">
@@ -122,18 +81,7 @@
                     </tbody>
                 </table>
             </div>
-            <h4 class="ui top attached block header"><i class="ui search icon"></i><?php echo $MSG_SEARCH;?></h4>
-            <div class="ui bottom attached segment">
-                <form action="problem.php" method="get">
-                    <div class="ui search" style="width: 100%; ">
-                        <div class="ui left icon input" style="width: 100%; ">
-                            <input class="prompt" style="width: 100%; " type="text" placeholder="<?php echo $MSG_PROBLEM_ID ;?> …" name="id">
-                            <i class="search icon"></i>
-                        </div>
-                        <div class="results" style="width: 100%; "></div>
-                    </div>
-                </form>
-            </div>
+            
             <h4 class="ui top attached block header"><i class="ui calendar icon"></i><?php echo $MSG_RECENT_CONTEST ;?></h4>
             <div class="ui bottom attached center aligned segment">
                 <table class="ui very basic center aligned table">
