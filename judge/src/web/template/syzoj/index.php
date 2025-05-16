ad20e6e8f34eda077e6fb29d410ef1b70a5c9c03
Son Seung Min <sonsmmm@naver.com>
2025-05-17 01:22:53 +0900

    이달의 우수생 fix

--- judge/src/web/template/syzoj/index.php
+++ judge/src/web/template/syzoj/index.php
@@ -72,25 +72,46 @@
           if(!empty($view_month_rank)) {
       ?>
             <h4 class="ui top attached block header"><i class="ui star icon"></i>이달의 우수생</h4>
-            <div class="ui bottom attached segment">
-              <table class="ui very basic center aligned table">
-                <tbody>
-                <?php foreach ($view_month_rank as $row) {
-                    echo "<tr><td><a href='userinfo.php?user=".htmlentities($row[0], ENT_QUOTES, "UTF-8")."'>".htmlentities($row[0], ENT_QUOTES, "UTF-8")."</a></td>
+                  <div class="ui bottom attached segment">
+        <table class="ui very basic center aligned table">
+          <thead>
+            <tr>
+              <th>순위</th>
+              <th>사용자 ID</th>
+              <th>닉네임</th>
+              <th>정답 문제 수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $medals = ['🥇', '🥈', '🥉'];
+            $rank = 1;
+            foreach ($view_month_rank as $row) {
+                $user_id = htmlentities($row[0], ENT_QUOTES, "UTF-8");
+                $nick = htmlentities($row[1], ENT_QUOTES, "UTF-8");
+                $ac_count = intval($row[2]);
+                $medal = ($rank <= 3) ? $medals[$rank - 1] : $rank;
 
-                        </tr>";
-                } ?>
-                </tbody>
-              </table>
-            </div>
+                echo "<tr>
+                        <td>{$medal}</td>
+                        <td><a href='userinfo.php?user={$user_id}'>{$user_id}</a></td>
+                        <td>{$nick}</td>
+                        <td>{$ac_count}</td>
+                      </tr>";
+                $rank++;
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+
       <?php }} ?>
+
+
     </div>
   </div>
 </div>
 
-<!-- 이달의 우수생 닉네임, 랭킹 -->
-<!-- <td>{$row[1]}</td>
-<td>{$row[2]}</td> -->
 
 <?php include("template/$OJ_TEMPLATE/footer.php"); ?>
 
