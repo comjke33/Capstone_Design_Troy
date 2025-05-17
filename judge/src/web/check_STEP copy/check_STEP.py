eb8fa3db7ec3a8e801fd67accc87cc671f49449d
taewoo <hst0092@inu.ac.kr>
2025-05-18 06:08:29 +0900

    줄바꿈

--- judge/src/web/check_STEP copy/check_STEP.py
+++ judge/src/web/check_STEP copy/check_STEP.py
@@ -130,14 +130,14 @@ def validate_code_output_full_io(code, test_in_path):
     exe_path = "/tmp/test_program"
     temp_c_path = "/tmp/final_code.c"
 
-    # 최종 코드 파일 작성 (이스케이프 없이 그대로 저장)
+    # 최종 코드 파일 작성
     with open(temp_c_path, 'w') as temp_file:
-        temp_file.write(code)
+        temp_file.write(code)  # 수정: 이스케이프 처리 없이 저장
 
     try:
         env = os.environ.copy()
         env["PATH"] = "/usr/lib/gcc/x86_64-linux-gnu/11:/usr/bin:/bin:/usr/sbin:/sbin:" + env.get("PATH", "")
-        result = subprocess.run(
+        subprocess.run(
             ['gcc', temp_c_path, '-o', exe_path],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -149,7 +149,7 @@ def validate_code_output_full_io(code, test_in_path):
         print(f"[❌] 컴파일 실패:\n{e.stderr}")
         return False
 
-    print("correct")
+
     return True
 
 def main():
@@ -166,6 +166,7 @@ def main():
     pid = params.get("problem_id")
     step = params.get("step")
     line_num = int(params.get("index"))
+    answer = params.get("answer")
     code_file = params.get("code_file")
 
     # 사용자 코드 불러오기 (파일로 직접 읽기)
