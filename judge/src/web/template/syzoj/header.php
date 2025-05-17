c5e4e08493f7e629b5c61f53f1fc59fe921f6c4f
Son Seung Min <sonsmmm@naver.com>
2025-05-17 23:34:43 +0900

    ranklist.php ui fix

--- judge/src/web/template/syzoj/header.php
+++ judge/src/web/template/syzoj/header.php
@@ -213,17 +213,18 @@ include('allowed_users.php');
             <?php } ?>
             
             <!-- 문제해결 전략게시판 -->
-             
+
             <!-- <a class="item <?php if ($url=="faqs.php") echo "active";?>" href="<?php echo $path_fix?>faqs.php"><i
                     class="help circle icon"></i><span class="desktop-only"> <?php echo $MSG_FAQ?></span></a>
 
             <a class="item <?php if ($url=="troy_debugging.php") echo "active";?>" href="<?php echo $path_fix?>troy_debugging.php"><i
             class="bug icon"></i><span class="desktop-only"> <?php echo "디버그용"?></span></a> -->
             
+
             <!-- 토론 게시판 -->
-              <?php if (isset($OJ_BBS)&& $OJ_BBS){ ?>
-                  <a class='item' href="discuss.php"><i class="clipboard icon"></i> <span class="desktop-only"><?php echo $MSG_BBS?></span></a>
-              <?php }
+            <?php if (isset($OJ_BBS)&& $OJ_BBS){ ?>
+                <a class='item' href="discuss.php"><i class="clipboard icon"></i> <span class="desktop-only"><php echo $MSG_BBS?></span></a>
+            <?php }
 
             }
                 ?>
@@ -245,7 +246,6 @@ include('allowed_users.php');
                     <?php }  ?>
             <?php }  ?>
             <?php echo $sql_news_menu_result_html; ?>
-
 
 
             <div class="right menu">
