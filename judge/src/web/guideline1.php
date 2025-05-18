805e4afda41561a6486e5cd1001eef6a3ba94bb1
Son Seung Min <sonsmmm@naver.com>
2025-05-19 03:03:01 +0900

    find error

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -9,8 +9,9 @@ $file_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_guideline/{$proble
 $tagged_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_code/{$problem_id}_step1.txt";
 
 $guideline_contents = file_get_contents($file_path);
-// $tagged_contents = file_get_contents($tagged_path);
-$tagged_contents = mb_convert_encoding($tagged_path, 'UTF-8', 'auto');
+$tagged_contents = file_get_contents($tagged_path); // 먼저 파일 내용을 읽고
+$tagged_contents = mb_convert_encoding($tagged_contents, 'UTF-8', 'auto'); // 그걸 인코딩 변환
+
 
 $OJ_BLOCK_TREE = guidelineFilter($guideline_contents);
 $OJ_CORRECT_ANSWERS = codeFilter($tagged_contents);
