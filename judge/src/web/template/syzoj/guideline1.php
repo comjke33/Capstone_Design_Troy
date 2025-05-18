162c819a77a10ee0a4fb5753f8d9983848cfd78c
taewoo <hst0092@inu.ac.kr>
2025-05-18 23:44:53 +0900

    줄바꿈

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -222,7 +222,8 @@ function submitAnswer(index) {
     console.log("요청 데이터:", {
         answer: input,
         problem_id: problemId,
-        index: index
+        index: index,
+        step:step
     });
 
     fetch("../../ajax/check_answer_STEP.php", {
@@ -231,7 +232,8 @@ function submitAnswer(index) {
         body: JSON.stringify({
             answer: input,
             problem_id: problemId,
-            index: index
+            index: index,
+            step:step
         })
     })
     .then(res => {
