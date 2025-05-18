c55514ce27e322463063163eed65e01803614908
Son Seung Min <sonsmmm@naver.com>
2025-05-19 03:01:39 +0900

    find error

--- judge/src/web/guideline1.php
+++ judge/src/web/guideline1.php
@@ -10,6 +10,7 @@ $tagged_path = "/home/Capstone_Design_Troy/judge/src/web/tagged_code/{$problem_i
 
 $guideline_contents = file_get_contents($file_path);
 $tagged_contents = file_get_contents($tagged_path);
+$tagged_contents = mb_convert_encoding($tagged_path, 'UTF-8', 'auto');
 
 $OJ_BLOCK_TREE = guidelineFilter($guideline_contents);
 $OJ_CORRECT_ANSWERS = codeFilter($tagged_contents);
