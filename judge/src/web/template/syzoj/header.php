27ca9fc27806b2af84480f1395a12b3155682a2d
Son Seung Min <sonsmmm@naver.com>
2025-05-21 22:46:44 +0900

    header.php 문제해결 전략 게시판 restore

--- judge/src/web/template/syzoj/header.php
+++ judge/src/web/template/syzoj/header.php
@@ -213,13 +213,13 @@ include('allowed_users.php');
                             href="<?php echo $path_fix?>recent-contest.php"><i class="bullhorn icon"></i> <span class="desktop-only"><?php echo $MSG_RECENT_CONTEST?></span></a>
             <?php } ?>
             
-            <!-- 문제해결 전략게시판 -->
+            <!-- 문제해결 전략게시판 --> -->
 
-            <!-- <a class="item <?php if ($url=="faqs.php") echo "active";?>" href="<?php echo $path_fix?>faqs.php"><i
+            <a class="item <?php if ($url=="faqs.php") echo "active";?>" href="<?php echo $path_fix?>faqs.php"><i
                     class="help circle icon"></i><span class="desktop-only"> <?php echo $MSG_FAQ?></span></a>
 
             <a class="item <?php if ($url=="troy_debugging.php") echo "active";?>" href="<?php echo $path_fix?>troy_debugging.php"><i
-            class="bug icon"></i><span class="desktop-only"> <?php echo "디버그용"?></span></a> -->
+            class="bug icon"></i><span class="desktop-only"> <?php echo "디버그용"?></span></a> 
             
 
             <!-- 토론 게시판 -->
