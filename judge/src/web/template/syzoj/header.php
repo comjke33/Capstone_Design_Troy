ec28c7ac96146ef7906c092218ba5f9b6511319a
Son Seung Min <sonsmmm@naver.com>
2025-05-22 03:32:50 +0900

    faqs.php->strategy.php

--- judge/src/web/template/syzoj/header.php
+++ judge/src/web/template/syzoj/header.php
@@ -215,7 +215,7 @@ include('allowed_users.php');
             
             <!-- 문제해결 전략게시판 -->
 
-            <a class="item <?php if ($url=="faqs.php") echo "active";?>" href="<?php echo $path_fix?>faqs.php"><i
+            <a class="item <?php if ($url=="strategy.php") echo "active";?>" href="<?php echo $path_fix?>strategy.php"><i
                     class="help circle icon"></i><span class="desktop-only"> <?php echo $MSG_FAQ?></span></a>
 
             <!-- 디버그용 -->
