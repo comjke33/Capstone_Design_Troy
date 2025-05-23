1f5997c8b92094a06ec1c4086188038247e11f38
Son Seung Min <sonsmmm@naver.com>
2025-05-23 15:54:15 +0900

    flowchart scroll add

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -36,9 +36,12 @@ include("../../guideline_common.php");
     <div class="left-panel">
     <div class="flowchart-wrapper active" id="flowchart-wrapper">
         <div class="flowchart-title">Flowchart</div>
-        <img id="flowchart_image">
+        <div class="flowchart-scroll">
+        <img id="flowchart_image" src="flowchart.png" alt="Flowchart" />
+        </div>
     </div>
     </div>
+
 
     <!-- 가운데 패널 -->
 <div class="center-panel">
