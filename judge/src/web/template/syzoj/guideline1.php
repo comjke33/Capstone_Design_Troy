82a820a3c66ddd947f3f4fd547656cf15d0084df
Son Seung Min <sonsmmm@naver.com>
2025-05-26 16:37:04 +0900

    guideline1.php fix

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -79,7 +79,11 @@ include("../../guideline_common.php");
                     $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' readonly style='{$readonlyStyle}'>{$default_value}</textarea>";
                 } else {
                     // 일반 입력 블록
-                    $escaped_line = htmlspecialchars($raw, ENT_QUOTES, 'UTF-8');
+
+                    //탭, 띄어쓰기 제거
+                    $trimmed_line = preg_replace('/^\s+/', '', $raw); // 맨 앞 공백 제거
+                    $escaped_line = htmlspecialchars($trimmed_line, ENT_QUOTES, 'UTF-8');
+
                     $html .= "<div class='code-line'>{$escaped_line}</div>";
                     $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' {$disabled}></textarea>";
 
