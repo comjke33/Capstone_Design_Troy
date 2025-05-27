7b2dcb72722e86336038a14a5e10db4c5d37c325
taewoo <hst0092@inu.ac.kr>
2025-05-27 11:31:01 +0900

    모범코드블럭추가

--- judge/src/web/aifeedback/aifeedback.py
+++ judge/src/web/aifeedback/aifeedback.py
@@ -122,7 +122,7 @@ def get_blocks(code_lines):
 
 def get_guideline(problem_id, block_index, step):
     """가이드라인 파일에서 특정 블럭을 추출 (step 가변)"""
-    guideline_path = f"/home/Capstone_Design_Troy/judge/src/web/tagged_code/{problem_id}_step{step}.txt"
+    guideline_path = f"/home/Capstone_Design_Troy/judge/src/web/tagged_guideline/{problem_id}_step{step}.txt"
     if os.path.exists(guideline_path):
         code_lines = read_code_lines(guideline_path)
         _, blocks, _, _, _ = get_blocks(code_lines)
@@ -133,9 +133,9 @@ def get_guideline(problem_id, block_index, step):
 
 def get_model_block(problem_id, block_index, step):
     """태그된 모범 코드에서 특정 블럭 추출"""
-    path = f"/home/Capstone_Design_Troy/judge/src/web/tagged_code/{problem_id}_step{step}.txt"
-    if os.path.exists(path):
-        code_lines = read_code_lines(path)
+    model_path = f"/home/Capstone_Design_Troy/judge/src/web/tagged_code/{problem_id}_step{step}.txt"
+    if os.path.exists(model_path):
+        code_lines = read_code_lines(model_path)
         _, blocks, _, _, _ = get_blocks(code_lines)
         if block_index < len(blocks):
             return ''.join(clean_block(blocks[block_index]))
