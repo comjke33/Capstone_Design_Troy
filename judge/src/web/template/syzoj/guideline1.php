71ae2b99f2f2989194a3414e058beab4ba0d6fb2
taewoo <hst0092@inu.ac.kr>
2025-05-27 19:42:05 +0900

    마크업

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -59,22 +59,22 @@ include("../../guideline_common.php");
                 "순회" => "배열이나 리스트를 처음부터 끝까지 접근하는 과정입니다. 예: for (int i = 0; i < n; i++)"
             ];
         
-            // 1단계: 툴팁 span을 삽입
+            // 1. 태그 먼저 넣기 (이스케이프 하지 않음)
             foreach ($term_map as $term => $desc) {
                 $escaped_desc = htmlspecialchars($desc, ENT_QUOTES, 'UTF-8');
                 $tooltip = '<span class="term-tooltip" data-content="' . $escaped_desc . '">' . $term . '</span>';
+                // 단어 단위 일치
                 $text = preg_replace('/\b' . preg_quote($term, '/') . '\b/u', $tooltip, $text);
             }
         
-            // 2단계: span 제외하고 escape
-            return preserve_html_tags_during_escape($text);
+            // 2. 나머지만 escape (툴팁 태그 제외)
+            return escape_except_spans($text);
         }
         
-        function preserve_html_tags_during_escape($input) {
-            return preg_replace_callback('/(<[^>]+>)|([^<]+)/s', function($matches) {
-                return isset($matches[1]) && $matches[1] !== '' 
-                    ? $matches[1]  // 태그는 그대로
-                    : htmlspecialchars($matches[2], ENT_NOQUOTES, 'UTF-8');  // 텍스트만 escape
+        function escape_except_spans($input) {
+            return preg_replace_callback('/(<span[^>]+>.*?<\/span>)|([^<]+)/s', function($matches) {
+                if (!empty($matches[1])) return $matches[1]; // span 태그는 그대로
+                return htmlspecialchars($matches[2], ENT_QUOTES | ENT_HTML5, 'UTF-8');
             }, $input);
         }
 
@@ -110,7 +110,7 @@ include("../../guideline_common.php");
                     // 일반 입력 블록
                     //$escaped_line = htmlspecialchars($raw, ENT_QUOTES, 'UTF-8');
                     //$escaped_line = highlight_terms_with_tooltip(htmlspecialchars($raw, ENT_QUOTES, 'UTF-8'));
-                    $escaped_line = preserve_html_tags_during_escape(highlight_terms_with_tooltip($raw));
+                    $escaped_line = highlight_terms_with_tooltip($raw);
                     $html .= "<div class='code-line'>{$escaped_line}</div>";
                     $html .= "<textarea id='ta_{$answer_index}' class='styled-textarea' data-index='{$answer_index}' {$disabled}></textarea>";
 
