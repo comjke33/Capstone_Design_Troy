d8be8517f00d4d658ef6023e060595bdb1f0253d
Son Seung Min <sonsmmm@naver.com>
2025-05-27 16:18:32 +0900

    apply guideline2.php

--- judge/src/web/template/syzoj/guideline2.php
+++ judge/src/web/template/syzoj/guideline2.php
@@ -1,4 +1,4 @@
-<link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet" />
+<!-- <link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet" />
 
 <style>
 /* header 내 텍스트에 Pretendard 적용 */
@@ -6,7 +6,7 @@
   font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui,
     Roboto, 'Helvetica Neue', 'Segoe UI', Arial, sans-serif !important;
 }
-</style>
+</style> -->
 
 <?php
 include("template/$OJ_TEMPLATE/header.php");
