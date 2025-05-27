d0ffe179e0c91a081b1be17b5defd85e1a4d6cf5
Son Seung Min <sonsmmm@naver.com>
2025-05-27 15:43:34 +0900

    guidleine2.php fix

--- judge/src/web/template/syzoj/guideline2.php
+++ judge/src/web/template/syzoj/guideline2.php
@@ -394,8 +394,6 @@ function showAnswer(index) {
 }
 
 
-
-
 function showFeedback(index) {
     const urlParams = new URLSearchParams(window.location.search);
     const problemId = urlParams.get("problem_id") || "0";
