648e7022f00b8f18caf443eda2494137bda64134
Choi Yeon Ho <cyh990218@naver.com>
2025-05-27 18:59:40 +0900

    위로가

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -643,17 +643,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 document.addEventListener("DOMContentLoaded", function () {
-  $('.term-tooltip').popup({
-    position: 'top center',
-    hoverable: true,
-    delay: { show: 300, hide: 100 },
-    onShow: function () {
-      $(this).css('cursor', 'none');
-    },
-    onHide: function () {
-      $(this).css('cursor', 'help'); // 원래대로 복원
-    }
-  });
+    $('.term-tooltip').popup({
+        position: 'top center',
+        distanceAway: 20,  // 기본값보다 더 멀리 떨어뜨림 (위쪽으로)
+        hoverable: true,
+        delay: { show: 300, hide: 100 },
+        onShow: function () {
+            $(this).css('cursor', 'none');
+        },
+        onHide: function () {
+            $(this).css('cursor', 'help');
+        }
+    });
 });
 
 </script>
