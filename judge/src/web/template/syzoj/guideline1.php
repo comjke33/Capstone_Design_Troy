32189f41b1f74c6b8fc930724352415c6e8748d7
taewoo <hst0092@inu.ac.kr>
2025-05-27 17:14:44 +0900

    마크업

--- judge/src/web/template/syzoj/guideline1.php
+++ judge/src/web/template/syzoj/guideline1.php
@@ -631,7 +631,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     $('.term-tooltip').popup({
         position: 'top center',
-        offset: 50,  // ← 기본보다 더 위로 10px 올림
+        offset: 100,  // ← 기본보다 더 위로 10px 올림
         hoverable: true,
         delay: { show: 300, hide: 100 }
     });
